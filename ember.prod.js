7b4045b30701c927b72fb2014d710a13f820433d
Tomster <tomster@emberjs.com>
2014-07-14 20:28:38 +0000

    Ember Bower Auto build for https://github.com/emberjs/ember.js/commits/ea5ffe0adcc45488c0abd289df2a57a7384773ba.

--- ember.prod.js
+++ ember.prod.js
@@ -5,7 +5,7 @@
  *            Portions Copyright 2008-2011 Apple Inc. All rights reserved.
  * @license   Licensed under MIT license
  *            See https://raw.github.com/emberjs/ember.js/master/LICENSE
- * @version   1.8.0-beta.1+canary.01d92432
+ * @version   1.8.0-beta.1+canary.ea5ffe0a
  */
 
 (function() {
@@ -12535,7 +12535,7 @@ define("ember-metal/core",
 
       @class Ember
       @static
-      @version 1.8.0-beta.1+canary.01d92432
+      @version 1.8.0-beta.1+canary.ea5ffe0a
     */
 
     if ('undefined' === typeof Ember) {
@@ -12562,10 +12562,10 @@ define("ember-metal/core",
     /**
       @property VERSION
       @type String
-      @default '1.8.0-beta.1+canary.01d92432'
+      @default '1.8.0-beta.1+canary.ea5ffe0a'
       @static
     */
-    Ember.VERSION = '1.8.0-beta.1+canary.01d92432';
+    Ember.VERSION = '1.8.0-beta.1+canary.ea5ffe0a';
 
     /**
       Standard environmental variables. You can define these in a global `EmberENV`
@@ -21698,12 +21698,9 @@ define("ember-routing/system/dsl",
         }
 
         var createSubRoutes = false;
-        if (Ember.FEATURES.isEnabled('ember-routing-consistent-resources')) {
+        
           createSubRoutes = true;
-        } else {
-          if (callback) { createSubRoutes = true; }
-        }
-
+        
         if (createSubRoutes) {
           var dsl = new DSL(name);
           route(dsl, 'loading');
