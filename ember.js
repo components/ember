e1752664a4550d5042d3d1039cc805b1b06c601d
Tomster <tomster@emberjs.com>
2014-07-16 04:57:06 +0000

    Ember Bower Auto build for https://github.com/emberjs/ember.js/commits/0fc78e873649c871446a734023f2022bb984774b.

--- ember.js
+++ ember.js
@@ -5,7 +5,7 @@
  *            Portions Copyright 2008-2011 Apple Inc. All rights reserved.
  * @license   Licensed under MIT license
  *            See https://raw.github.com/emberjs/ember.js/master/LICENSE
- * @version   1.7.0-beta.2+pre.76dcf660
+ * @version   1.7.0-beta.2
  */
 
 (function() {
@@ -12816,7 +12816,7 @@ define("ember-metal/core",
 
       @class Ember
       @static
-      @version 1.7.0-beta.2+pre.76dcf660
+      @version 1.7.0-beta.2
     */
 
     if ('undefined' === typeof Ember) {
@@ -12843,10 +12843,10 @@ define("ember-metal/core",
     /**
       @property VERSION
       @type String
-      @default '1.7.0-beta.2+pre.76dcf660'
+      @default '1.7.0-beta.2'
       @static
     */
-    Ember.VERSION = '1.7.0-beta.2+pre.76dcf660';
+    Ember.VERSION = '1.7.0-beta.2';
 
     /**
       Standard environmental variables. You can define these in a global `EmberENV`
