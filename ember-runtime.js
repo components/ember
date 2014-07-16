b1c365e037ab9635b73961a4278c5d18a3ffdbbf
Tomster <tomster@emberjs.com>
2014-07-16 22:00:22 +0000

    Ember Bower Auto build for https://github.com/emberjs/ember.js/commits/d7eb3be12ad13bb4df4a7fd4e88e24b38b4d864c.

--- ember-runtime.js
+++ ember-runtime.js
@@ -5,7 +5,7 @@
  *            Portions Copyright 2008-2011 Apple Inc. All rights reserved.
  * @license   Licensed under MIT license
  *            See https://raw.github.com/emberjs/ember.js/master/LICENSE
- * @version   1.8.0-beta.1+canary.2b5f9b05
+ * @version   1.8.0-beta.1+canary.d7eb3be1
  */
 
 (function() {
@@ -4447,7 +4447,7 @@ define("ember-metal/core",
 
       @class Ember
       @static
-      @version 1.8.0-beta.1+canary.2b5f9b05
+      @version 1.8.0-beta.1+canary.d7eb3be1
     */
 
     if ('undefined' === typeof Ember) {
@@ -4474,10 +4474,10 @@ define("ember-metal/core",
     /**
       @property VERSION
       @type String
-      @default '1.8.0-beta.1+canary.2b5f9b05'
+      @default '1.8.0-beta.1+canary.d7eb3be1'
       @static
     */
-    Ember.VERSION = '1.8.0-beta.1+canary.2b5f9b05';
+    Ember.VERSION = '1.8.0-beta.1+canary.d7eb3be1';
 
     /**
       Standard environmental variables. You can define these in a global `EmberENV`
@@ -8603,7 +8603,7 @@ define("ember-metal/run_loop",
       ```
 
       @method bind
-      @namespace run
+      @namespace Ember
       @param {Object} [target] target of method to call
       @param {Function|String} method Method to invoke.
         May be a function or a string. If you pass a string
