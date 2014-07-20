8b13cb35af0c59fae36bfaea83e25666534c7a0d
Tomster <tomster@emberjs.com>
2014-07-20 15:40:16 +0000

    Ember Bower Auto build for https://github.com/emberjs/ember.js/commits/d98d0c980a0f0227c222c2bea5eccde438c05506.

--- ember-tests.js
+++ ember-tests.js
@@ -5,7 +5,7 @@
  *            Portions Copyright 2008-2011 Apple Inc. All rights reserved.
  * @license   Licensed under MIT license
  *            See https://raw.github.com/emberjs/ember.js/master/LICENSE
- * @version   1.8.0-beta.1+canary.dba0472d
+ * @version   1.8.0-beta.1+canary.d98d0c98
  */
 
 (function() {
--- ember.js
+++ ember.js
@@ -5,7 +5,7 @@
  *            Portions Copyright 2008-2011 Apple Inc. All rights reserved.
  * @license   Licensed under MIT license
  *            See https://raw.github.com/emberjs/ember.js/master/LICENSE
- * @version   1.8.0-beta.1+canary.dba0472d
+ * @version   1.8.0-beta.1+canary.d98d0c98
  */
 
 (function() {
@@ -12841,7 +12841,7 @@ define("ember-metal/core",
 
       @class Ember
       @static
-      @version 1.8.0-beta.1+canary.dba0472d
+      @version 1.8.0-beta.1+canary.d98d0c98
     */
 
     if ('undefined' === typeof Ember) {
@@ -12868,10 +12868,10 @@ define("ember-metal/core",
     /**
       @property VERSION
       @type String
-      @default '1.8.0-beta.1+canary.dba0472d'
+      @default '1.8.0-beta.1+canary.d98d0c98'
       @static
     */
-    Ember.VERSION = '1.8.0-beta.1+canary.dba0472d';
+    Ember.VERSION = '1.8.0-beta.1+canary.d98d0c98';
 
     /**
       Standard environmental variables. You can define these in a global `EmberENV`
