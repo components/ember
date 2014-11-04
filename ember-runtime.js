e532ac2742d9982cfd5f4de6fae2dbb5cb624bc2
Tomster <tomster@emberjs.com>
2014-11-04 22:45:28 +0000

    Ember Bower Auto build for https://github.com/emberjs/ember.js/commits/1a14916e45c69dac52773db36ec880ba129394a7.

--- ember-runtime.js
+++ ember-runtime.js
@@ -5,7 +5,7 @@
  *            Portions Copyright 2008-2011 Apple Inc. All rights reserved.
  * @license   Licensed under MIT license
  *            See https://raw.github.com/emberjs/ember.js/master/LICENSE
- * @version   1.10.0-beta.1+canary.69b57019
+ * @version   1.10.0-beta.1+canary.1a14916e
  */
 
 (function() {
@@ -4756,7 +4756,7 @@ define("ember-metal/core",
 
       @class Ember
       @static
-      @version 1.10.0-beta.1+canary.69b57019
+      @version 1.10.0-beta.1+canary.1a14916e
     */
 
     if ('undefined' === typeof Ember) {
@@ -4783,10 +4783,10 @@ define("ember-metal/core",
     /**
       @property VERSION
       @type String
-      @default '1.10.0-beta.1+canary.69b57019'
+      @default '1.10.0-beta.1+canary.1a14916e'
       @static
     */
-    Ember.VERSION = '1.10.0-beta.1+canary.69b57019';
+    Ember.VERSION = '1.10.0-beta.1+canary.1a14916e';
 
     /**
       Standard environmental variables. You can define these in a global `EmberENV`
