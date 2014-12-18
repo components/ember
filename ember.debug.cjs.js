7fb4ed75fb176ea28daca3612872c3981a79969d
Tomster <tomster@emberjs.com>
2014-12-18 22:05:21 +0000

    Ember Bower Auto build for https://github.com/emberjs/ember.js/commits/b97356564ad245aab81a9dca44338eed52cea73e.

--- ember.debug.cjs.js
+++ ember.debug.cjs.js
@@ -5,7 +5,7 @@
  *            Portions Copyright 2008-2011 Apple Inc. All rights reserved.
  * @license   Licensed under MIT license
  *            See https://raw.github.com/emberjs/ember.js/master/LICENSE
- * @version   1.11.0-beta.1+canary.f6d9ff91
+ * @version   1.11.0-beta.1+canary.b9735656
  */
 
 (function() {
@@ -12310,7 +12310,7 @@ enifed("ember-metal/core",
 
       @class Ember
       @static
-      @version 1.11.0-beta.1+canary.f6d9ff91
+      @version 1.11.0-beta.1+canary.b9735656
     */
 
     if ('undefined' === typeof Ember) {
@@ -12337,10 +12337,10 @@ enifed("ember-metal/core",
     /**
       @property VERSION
       @type String
-      @default '1.11.0-beta.1+canary.f6d9ff91'
+      @default '1.11.0-beta.1+canary.b9735656'
       @static
     */
-    Ember.VERSION = '1.11.0-beta.1+canary.f6d9ff91';
+    Ember.VERSION = '1.11.0-beta.1+canary.b9735656';
 
     /**
       Standard environmental variables. You can define these in a global `EmberENV`
--- ember.prod.js
+++ ember.prod.js
@@ -5,7 +5,7 @@
  *            Portions Copyright 2008-2011 Apple Inc. All rights reserved.
  * @license   Licensed under MIT license
  *            See https://raw.github.com/emberjs/ember.js/master/LICENSE
- * @version   1.11.0-beta.1+canary.f6d9ff91
+ * @version   1.11.0-beta.1+canary.b9735656
  */
 
 (function() {
@@ -11908,7 +11908,7 @@ enifed("ember-metal/core",
 
       @class Ember
       @static
-      @version 1.11.0-beta.1+canary.f6d9ff91
+      @version 1.11.0-beta.1+canary.b9735656
     */
 
     if ('undefined' === typeof Ember) {
@@ -11935,10 +11935,10 @@ enifed("ember-metal/core",
     /**
       @property VERSION
       @type String
-      @default '1.11.0-beta.1+canary.f6d9ff91'
+      @default '1.11.0-beta.1+canary.b9735656'
       @static
     */
-    Ember.VERSION = '1.11.0-beta.1+canary.f6d9ff91';
+    Ember.VERSION = '1.11.0-beta.1+canary.b9735656';
 
     /**
       Standard environmental variables. You can define these in a global `EmberENV`
