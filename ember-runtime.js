c28b7dfba4fc7bcc63913d586f756d5108f4579e
Tomster <tomster@emberjs.com>
2015-01-03 06:59:29 +0000

    Ember Bower Auto build for https://github.com/emberjs/ember.js/commits/f181351212013e56b287091f5a9fbf0143f57a22.

--- ember-runtime.js
+++ ember-runtime.js
@@ -5,7 +5,7 @@
  *            Portions Copyright 2008-2011 Apple Inc. All rights reserved.
  * @license   Licensed under MIT license
  *            See https://raw.github.com/emberjs/ember.js/master/LICENSE
- * @version   1.11.0-beta.1+canary.eff10145
+ * @version   1.11.0-beta.1+canary.f1813512
  */
 
 (function() {
@@ -5022,7 +5022,7 @@ enifed("ember-metal/core",
 
       @class Ember
       @static
-      @version 1.11.0-beta.1+canary.eff10145
+      @version 1.11.0-beta.1+canary.f1813512
     */
 
     if ('undefined' === typeof Ember) {
@@ -5049,10 +5049,10 @@ enifed("ember-metal/core",
     /**
       @property VERSION
       @type String
-      @default '1.11.0-beta.1+canary.eff10145'
+      @default '1.11.0-beta.1+canary.f1813512'
       @static
     */
-    Ember.VERSION = '1.11.0-beta.1+canary.eff10145';
+    Ember.VERSION = '1.11.0-beta.1+canary.f1813512';
 
     /**
       Standard environmental variables. You can define these in a global `EmberENV`
@@ -20539,8 +20539,8 @@ enifed("ember-runtime/system/core_object",
         view.get('classNames'); // ['ember-view', 'bar', 'foo', 'baz']
         ```
 
-        Using the `concatenatedProperties` property, we can tell to Ember that mix
-        the content of the properties.
+        Using the `concatenatedProperties` property, we can tell Ember to mix the
+        content of the properties.
 
         In `Ember.View` the `classNameBindings` and `attributeBindings` properties
         are also concatenated, in addition to `classNames`.
--- ember-template-compiler.js
+++ ember-template-compiler.js
@@ -5,7 +5,7 @@
  *            Portions Copyright 2008-2011 Apple Inc. All rights reserved.
  * @license   Licensed under MIT license
  *            See https://raw.github.com/emberjs/ember.js/master/LICENSE
- * @version   1.11.0-beta.1+canary.eff10145
+ * @version   1.11.0-beta.1+canary.f1813512
  */
 
 (function() {
@@ -119,7 +119,7 @@ enifed("ember-metal/core",
 
       @class Ember
       @static
-      @version 1.11.0-beta.1+canary.eff10145
+      @version 1.11.0-beta.1+canary.f1813512
     */
 
     if ('undefined' === typeof Ember) {
@@ -146,10 +146,10 @@ enifed("ember-metal/core",
     /**
       @property VERSION
       @type String
-      @default '1.11.0-beta.1+canary.eff10145'
+      @default '1.11.0-beta.1+canary.f1813512'
       @static
     */
-    Ember.VERSION = '1.11.0-beta.1+canary.eff10145';
+    Ember.VERSION = '1.11.0-beta.1+canary.f1813512';
 
     /**
       Standard environmental variables. You can define these in a global `EmberENV`
