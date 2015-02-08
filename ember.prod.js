e97176d98338058bebdf5bb927c27946831c3a30
Tomster <tomster@emberjs.com>
2015-02-08 19:24:52 +0000

    Ember Bower Auto build for https://github.com/emberjs/ember.js/commits/caa7874b592c554df23ae4292098374443094d2d.

--- ember.prod.js
+++ ember.prod.js
@@ -5,7 +5,7 @@
  *            Portions Copyright 2008-2011 Apple Inc. All rights reserved.
  * @license   Licensed under MIT license
  *            See https://raw.github.com/emberjs/ember.js/master/LICENSE
- * @version   1.12.0-beta.1+canary.b186e6e3
+ * @version   1.12.0-beta.1+canary.caa7874b
  */
 
 (function() {
@@ -10932,7 +10932,7 @@ enifed('ember-metal/core', ['exports'], function (exports) {
 
     @class Ember
     @static
-    @version 1.12.0-beta.1+canary.b186e6e3
+    @version 1.12.0-beta.1+canary.caa7874b
   */
 
   if ('undefined' === typeof Ember) {
@@ -10960,10 +10960,10 @@ enifed('ember-metal/core', ['exports'], function (exports) {
   /**
     @property VERSION
     @type String
-    @default '1.12.0-beta.1+canary.b186e6e3'
+    @default '1.12.0-beta.1+canary.caa7874b'
     @static
   */
-  Ember.VERSION = '1.12.0-beta.1+canary.b186e6e3';
+  Ember.VERSION = '1.12.0-beta.1+canary.caa7874b';
 
   /**
     Standard environmental variables. You can define these in a global `EmberENV`
@@ -23497,7 +23497,7 @@ enifed('ember-routing/system/router', ['exports', 'ember-metal/core', 'ember-met
     var target;
     var myState = {
       render: renderOptions,
-      outlets: Object.create(null)
+      outlets: create['default'](null)
     };
     if (!parentRoute) {
       liveRoutes = myState;
