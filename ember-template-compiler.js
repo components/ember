52dce38f1a39dff8a9827bf0b6cf8620bd0bca99
Tomster <tomster@emberjs.com>
2015-02-12 13:39:16 +0000

    Ember Bower Auto build for https://github.com/emberjs/ember.js/commits/f067a37b0d1722613693a3d73209f335377a7c64.

--- ember-template-compiler.js
+++ ember-template-compiler.js
@@ -5,7 +5,7 @@
  *            Portions Copyright 2008-2011 Apple Inc. All rights reserved.
  * @license   Licensed under MIT license
  *            See https://raw.github.com/emberjs/ember.js/master/LICENSE
- * @version   1.12.0-beta.1+canary.9bbb667d
+ * @version   1.12.0-beta.1+canary.f067a37b
  */
 
 (function() {
@@ -133,7 +133,7 @@ enifed('ember-metal/core', ['exports'], function (exports) {
 
     @class Ember
     @static
-    @version 1.12.0-beta.1+canary.9bbb667d
+    @version 1.12.0-beta.1+canary.f067a37b
   */
 
   if ('undefined' === typeof Ember) {
@@ -161,10 +161,10 @@ enifed('ember-metal/core', ['exports'], function (exports) {
   /**
     @property VERSION
     @type String
-    @default '1.12.0-beta.1+canary.9bbb667d'
+    @default '1.12.0-beta.1+canary.f067a37b'
     @static
   */
-  Ember.VERSION = '1.12.0-beta.1+canary.9bbb667d';
+  Ember.VERSION = '1.12.0-beta.1+canary.f067a37b';
 
   /**
     Standard environmental variables. You can define these in a global `EmberENV`
--- ember.debug.cjs.js
+++ ember.debug.cjs.js
@@ -5,7 +5,7 @@
  *            Portions Copyright 2008-2011 Apple Inc. All rights reserved.
  * @license   Licensed under MIT license
  *            See https://raw.github.com/emberjs/ember.js/master/LICENSE
- * @version   1.12.0-beta.1+canary.9bbb667d
+ * @version   1.12.0-beta.1+canary.f067a37b
  */
 
 (function() {
@@ -11339,7 +11339,7 @@ enifed('ember-metal/core', ['exports'], function (exports) {
 
     @class Ember
     @static
-    @version 1.12.0-beta.1+canary.9bbb667d
+    @version 1.12.0-beta.1+canary.f067a37b
   */
 
   if ('undefined' === typeof Ember) {
@@ -11367,10 +11367,10 @@ enifed('ember-metal/core', ['exports'], function (exports) {
   /**
     @property VERSION
     @type String
-    @default '1.12.0-beta.1+canary.9bbb667d'
+    @default '1.12.0-beta.1+canary.f067a37b'
     @static
   */
-  Ember.VERSION = '1.12.0-beta.1+canary.9bbb667d';
+  Ember.VERSION = '1.12.0-beta.1+canary.f067a37b';
 
   /**
     Standard environmental variables. You can define these in a global `EmberENV`
@@ -41484,7 +41484,7 @@ enifed('ember-views/views/text_field', ['exports', 'ember-views/views/component'
   });
 
 });
-enifed('ember-views/views/view', ['exports', 'ember-metal/core', 'ember-runtime/mixins/evented', 'ember-runtime/system/object', 'ember-metal/error', 'ember-metal/property_get', 'ember-metal/run_loop', 'ember-metal/observer', 'ember-metal/utils', 'ember-metal/computed', 'ember-metal/mixin', 'ember-metal/deprecate_property', 'ember-metal/property_events', 'ember-views/system/jquery', 'ember-views/system/ext', 'ember-views/views/core_view', 'ember-views/mixins/view_stream_support', 'ember-views/mixins/view_keyword_support', 'ember-views/mixins/view_context_support', 'ember-views/mixins/view_child_views_support', 'ember-views/mixins/view_state_support', 'ember-views/mixins/template_rendering_support', 'ember-views/mixins/class_names_support', 'ember-views/mixins/attribute_bindings_support', 'ember-views/mixins/legacy_view_support', 'ember-views/mixins/instrumentation_support', 'ember-views/mixins/visibility_support'], function (exports, Ember, Evented, EmberObject, EmberError, property_get, run, observer, utils, computed, mixin, deprecate_property, property_events, jQuery, __dep13__, CoreView, ViewStreamSupport, ViewKeywordSupport, ViewContextSupport, view_child_views_support, ViewStateSupport, TemplateRenderingSupport, ClassNamesSupport, AttributeBindingsSupport, LegacyViewSupport, InstrumentationSupport, VisibilitySupport) {
+enifed('ember-views/views/view', ['exports', 'ember-metal/core', 'ember-metal/error', 'ember-metal/property_get', 'ember-metal/run_loop', 'ember-metal/observer', 'ember-metal/utils', 'ember-metal/computed', 'ember-metal/mixin', 'ember-metal/deprecate_property', 'ember-metal/property_events', 'ember-views/system/jquery', 'ember-views/system/ext', 'ember-views/views/core_view', 'ember-views/mixins/view_stream_support', 'ember-views/mixins/view_keyword_support', 'ember-views/mixins/view_context_support', 'ember-views/mixins/view_child_views_support', 'ember-views/mixins/view_state_support', 'ember-views/mixins/template_rendering_support', 'ember-views/mixins/class_names_support', 'ember-views/mixins/attribute_bindings_support', 'ember-views/mixins/legacy_view_support', 'ember-views/mixins/instrumentation_support', 'ember-views/mixins/visibility_support'], function (exports, Ember, EmberError, property_get, run, observer, utils, computed, mixin, deprecate_property, property_events, jQuery, __dep11__, CoreView, ViewStreamSupport, ViewKeywordSupport, ViewContextSupport, view_child_views_support, ViewStateSupport, TemplateRenderingSupport, ClassNamesSupport, AttributeBindingsSupport, LegacyViewSupport, InstrumentationSupport, VisibilitySupport) {
 
   'use strict';
 
@@ -42356,21 +42356,6 @@ enifed('ember-views/views/view', ['exports', 'ember-metal/core', 'ember-runtime/
       return this.currentState.rerender(this);
     },
 
-    /**
-      Given a property name, returns a dasherized version of that
-      property name if the property evaluates to a non-falsy value.
-
-      For example, if the view has property `isUrgent` that evaluates to true,
-      passing `isUrgent` to this method will return `"is-urgent"`.
-
-      @method _classStringForProperty
-      @param property
-      @private
-    */
-    _classStringForProperty: function(parsedPath) {
-      return View._classStringForValue(parsedPath.path, parsedPath.stream.value(), parsedPath.className, parsedPath.falsyClassName);
-    },
-
     // ..........................................................
     // ELEMENT SUPPORT
     //
@@ -42832,20 +42817,6 @@ enifed('ember-views/views/view', ['exports', 'ember-metal/core', 'ember-runtime/
 
     // once the view has been inserted into the DOM, legal manipulations
     // are done on the DOM element.
-
-  var mutation = EmberObject['default'].extend(Evented['default']).create();
-  // TODO MOVE TO RENDERER HOOKS
-  View.addMutationListener = function(callback) {
-    mutation.on('change', callback);
-  };
-
-  View.removeMutationListener = function(callback) {
-    mutation.off('change', callback);
-  };
-
-  View.notifyMutationListeners = function() {
-    mutation.trigger('change');
-  };
 
   /**
     Global views hash
