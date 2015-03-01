470f84b68e4afc5f5cf70d5b316932b65b9c1cb9
Tomster <tomster@emberjs.com>
2015-03-01 06:49:29 +0000

    Ember Bower Auto build for https://github.com/emberjs/ember.js/commits/a4140e2969a49c2be5a406a849275d366da7c89f.

--- ember-tests.js
+++ ember-tests.js
@@ -5,7 +5,7 @@
  *            Portions Copyright 2008-2011 Apple Inc. All rights reserved.
  * @license   Licensed under MIT license
  *            See https://raw.github.com/emberjs/ember.js/master/LICENSE
- * @version   1.11.0-beta.3.6f4cddc3
+ * @version   1.11.0-beta.4
  */
 
 (function() {
@@ -16961,7 +16961,7 @@ enifed('ember-htmlbars/tests/system/render_view_test', ['ember-runtime/tests/uti
     view = EmberView['default'].create({
       template: {
         isHTMLBars: true,
-        revision: 'Ember@1.11.0-beta.3.6f4cddc3',
+        revision: 'Ember@1.11.0-beta.4',
         render: function(view, env, contextualElement, blockArguments) {
           for (var i = 0, l = keyNames.length; i < l; i++) {
             var keyName = keyNames[i];
@@ -52185,7 +52185,7 @@ enifed('ember-template-compiler/tests/system/compile_test', ['ember-template-com
 
     var actual = compile['default'](templateString);
 
-    equal(actual.revision, 'Ember@1.11.0-beta.3.6f4cddc3', 'revision is included in generated template');
+    equal(actual.revision, 'Ember@1.11.0-beta.4', 'revision is included in generated template');
   });
 
   QUnit.test('the template revision is different than the HTMLBars default revision', function() {
