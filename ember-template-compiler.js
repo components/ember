7efb75c0156e61bf1be018f94d5968e945992e7e
Tomster <tomster@emberjs.com>
2015-04-03 06:35:38 +0000

    Ember Bower Auto build for https://github.com/emberjs/ember.js/commits/372adee82a5c28279fc377363a3f812f10f41f9c.

--- ember-template-compiler.js
+++ ember-template-compiler.js
@@ -5,7 +5,7 @@
  *            Portions Copyright 2008-2011 Apple Inc. All rights reserved.
  * @license   Licensed under MIT license
  *            See https://raw.github.com/emberjs/ember.js/master/LICENSE
- * @version   1.13.0-beta.1+canary.10b32385
+ * @version   1.13.0-beta.1+canary.372adee8
  */
 
 (function() {
@@ -2610,7 +2610,7 @@ enifed('ember-metal/core', ['exports'], function (exports) {
 
     @class Ember
     @static
-    @version 1.13.0-beta.1+canary.10b32385
+    @version 1.13.0-beta.1+canary.372adee8
   */
 
   if ('undefined' === typeof Ember) {
@@ -2639,10 +2639,10 @@ enifed('ember-metal/core', ['exports'], function (exports) {
   /**
     @property VERSION
     @type String
-    @default '1.13.0-beta.1+canary.10b32385'
+    @default '1.13.0-beta.1+canary.372adee8'
     @static
   */
-  Ember.VERSION = '1.13.0-beta.1+canary.10b32385';
+  Ember.VERSION = '1.13.0-beta.1+canary.372adee8';
 
   /**
     Standard environmental variables. You can define these in a global `EmberENV`
@@ -9366,7 +9366,7 @@ enifed('ember-template-compiler/system/compile_options', ['exports', 'ember-meta
     }
 
     return {
-      revision: "Ember@1.13.0-beta.1+canary.10b32385",
+      revision: "Ember@1.13.0-beta.1+canary.372adee8",
 
       disableComponentGeneration: disableComponentGeneration,
 
--- ember-tests.js
+++ ember-tests.js
@@ -5,7 +5,7 @@
  *            Portions Copyright 2008-2011 Apple Inc. All rights reserved.
  * @license   Licensed under MIT license
  *            See https://raw.github.com/emberjs/ember.js/master/LICENSE
- * @version   1.13.0-beta.1+canary.10b32385
+ * @version   1.13.0-beta.1+canary.372adee8
  */
 
 (function() {
@@ -17774,7 +17774,7 @@ enifed('ember-htmlbars/tests/system/render_view_test', ['ember-runtime/tests/uti
     view = EmberView['default'].create({
       template: {
         isHTMLBars: true,
-        revision: "Ember@1.13.0-beta.1+canary.10b32385",
+        revision: "Ember@1.13.0-beta.1+canary.372adee8",
         render: function (view, env, contextualElement, blockArguments) {
           for (var i = 0, l = keyNames.length; i < l; i++) {
             var keyName = keyNames[i];
@@ -53432,7 +53432,7 @@ enifed('ember-template-compiler/tests/system/compile_test', ['ember-template-com
 
     var actual = compile['default'](templateString);
 
-    equal(actual.revision, "Ember@1.13.0-beta.1+canary.10b32385", "revision is included in generated template");
+    equal(actual.revision, "Ember@1.13.0-beta.1+canary.372adee8", "revision is included in generated template");
   });
 
   QUnit.test("the template revision is different than the HTMLBars default revision", function () {
