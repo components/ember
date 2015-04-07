a14c9b947f324bdc8530d70721c7878356b00aba
Tomster <tomster@emberjs.com>
2015-04-07 00:54:38 +0000

    Ember Bower Auto build for https://github.com/emberjs/ember.js/commits/bad982af79b931f962ed4e0c03cfa2079066c434.

--- ember-runtime.js
+++ ember-runtime.js
@@ -5,7 +5,7 @@
  *            Portions Copyright 2008-2011 Apple Inc. All rights reserved.
  * @license   Licensed under MIT license
  *            See https://raw.github.com/emberjs/ember.js/master/LICENSE
- * @version   1.13.0-beta.1+canary.1a185ef1
+ * @version   1.13.0-beta.1+canary.bad982af
  */
 
 (function() {
@@ -4382,7 +4382,7 @@ enifed('ember-metal/core', ['exports'], function (exports) {
 
     @class Ember
     @static
-    @version 1.13.0-beta.1+canary.1a185ef1
+    @version 1.13.0-beta.1+canary.bad982af
   */
 
   if ('undefined' === typeof Ember) {
@@ -4411,10 +4411,10 @@ enifed('ember-metal/core', ['exports'], function (exports) {
   /**
     @property VERSION
     @type String
-    @default '1.13.0-beta.1+canary.1a185ef1'
+    @default '1.13.0-beta.1+canary.bad982af'
     @static
   */
-  Ember.VERSION = '1.13.0-beta.1+canary.1a185ef1';
+  Ember.VERSION = '1.13.0-beta.1+canary.bad982af';
 
   /**
     Standard environmental variables. You can define these in a global `EmberENV`
--- ember-template-compiler.js
+++ ember-template-compiler.js
@@ -5,7 +5,7 @@
  *            Portions Copyright 2008-2011 Apple Inc. All rights reserved.
  * @license   Licensed under MIT license
  *            See https://raw.github.com/emberjs/ember.js/master/LICENSE
- * @version   1.13.0-beta.1+canary.1a185ef1
+ * @version   1.13.0-beta.1+canary.bad982af
  */
 
 (function() {
@@ -2610,7 +2610,7 @@ enifed('ember-metal/core', ['exports'], function (exports) {
 
     @class Ember
     @static
-    @version 1.13.0-beta.1+canary.1a185ef1
+    @version 1.13.0-beta.1+canary.bad982af
   */
 
   if ('undefined' === typeof Ember) {
@@ -2639,10 +2639,10 @@ enifed('ember-metal/core', ['exports'], function (exports) {
   /**
     @property VERSION
     @type String
-    @default '1.13.0-beta.1+canary.1a185ef1'
+    @default '1.13.0-beta.1+canary.bad982af'
     @static
   */
-  Ember.VERSION = '1.13.0-beta.1+canary.1a185ef1';
+  Ember.VERSION = '1.13.0-beta.1+canary.bad982af';
 
   /**
     Standard environmental variables. You can define these in a global `EmberENV`
@@ -9371,7 +9371,7 @@ enifed('ember-template-compiler/system/compile_options', ['exports', 'ember-meta
     }
 
     return {
-      revision: "Ember@1.13.0-beta.1+canary.1a185ef1",
+      revision: "Ember@1.13.0-beta.1+canary.bad982af",
 
       disableComponentGeneration: disableComponentGeneration,
 
