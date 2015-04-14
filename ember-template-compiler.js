34bc8c96ce3f6a71e580185a23e37a27dccb4c9f
Robert Jackson <robert.w.jackson@me.com>
2015-04-14 16:32:48 -0400

    Release 1.10.1

--- ember-template-compiler.js
+++ ember-template-compiler.js
@@ -5,7 +5,7 @@
  *            Portions Copyright 2008-2011 Apple Inc. All rights reserved.
  * @license   Licensed under MIT license
  *            See https://raw.github.com/emberjs/ember.js/master/LICENSE
- * @version   1.10.0
+ * @version   1.10.1
  */
 
 (function() {
@@ -114,7 +114,7 @@ define("ember-metal/core",
 
       @class Ember
       @static
-      @version 1.10.0
+      @version 1.10.1
     */
 
     if ('undefined' === typeof Ember) {
@@ -141,10 +141,10 @@ define("ember-metal/core",
     /**
       @property VERSION
       @type String
-      @default '1.10.0'
+      @default '1.10.1'
       @static
     */
-    Ember.VERSION = '1.10.0';
+    Ember.VERSION = '1.10.1';
 
     /**
       Standard environmental variables. You can define these in a global `EmberENV`
