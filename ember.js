515c54888870efc547b7f8ecd938a9270063f474
Tomster <tomster@emberjs.com>
2015-05-15 00:47:37 +0000

    Ember Bower Auto build for https://github.com/emberjs/ember.js/commits/5b3b67e539349438aa17c614ae5c0bc2c071a795.

--- ember.js
+++ ember.js
@@ -5,7 +5,7 @@
  *            Portions Copyright 2008-2011 Apple Inc. All rights reserved.
  * @license   Licensed under MIT license
  *            See https://raw.github.com/emberjs/ember.js/master/LICENSE
- * @version   2.0.0-beta.1+canary.23423536
+ * @version   2.0.0-beta.1+canary.5b3b67e5
  */
 
 (function() {
@@ -8097,7 +8097,7 @@ enifed('ember-htmlbars/keywords/real_outlet', ['exports', 'ember-metal/property_
   @submodule ember-htmlbars
   */
 
-  topLevelViewTemplate['default'].meta.revision = "Ember@2.0.0-beta.1+canary.23423536";
+  topLevelViewTemplate['default'].meta.revision = "Ember@2.0.0-beta.1+canary.5b3b67e5";
 
   exports['default'] = {
     willRender: function (renderNode, env) {
@@ -12997,7 +12997,7 @@ enifed('ember-metal/core', ['exports'], function (exports) {
 
     @class Ember
     @static
-    @version 2.0.0-beta.1+canary.23423536
+    @version 2.0.0-beta.1+canary.5b3b67e5
   */
 
   if ('undefined' === typeof Ember) {
@@ -13026,10 +13026,10 @@ enifed('ember-metal/core', ['exports'], function (exports) {
   /**
     @property VERSION
     @type String
-    @default '2.0.0-beta.1+canary.23423536'
+    @default '2.0.0-beta.1+canary.5b3b67e5'
     @static
   */
-  Ember.VERSION = '2.0.0-beta.1+canary.23423536';
+  Ember.VERSION = '2.0.0-beta.1+canary.5b3b67e5';
 
   /**
     Standard environmental variables. You can define these in a global `EmberENV`
@@ -20380,7 +20380,7 @@ enifed('ember-routing-views/views/link', ['exports', 'ember-metal/core', 'ember-
   @submodule ember-routing-views
   */
 
-  linkToTemplate['default'].meta.revision = "Ember@2.0.0-beta.1+canary.23423536";
+  linkToTemplate['default'].meta.revision = "Ember@2.0.0-beta.1+canary.5b3b67e5";
 
   var linkViewClassNameBindings = ["active", "loading", "disabled"];
   
@@ -20854,7 +20854,7 @@ enifed('ember-routing-views/views/outlet', ['exports', 'ember-views/views/view',
   @submodule ember-routing-views
   */
 
-  topLevelViewTemplate['default'].meta.revision = "Ember@2.0.0-beta.1+canary.23423536";
+  topLevelViewTemplate['default'].meta.revision = "Ember@2.0.0-beta.1+canary.5b3b67e5";
 
   var CoreOutletView = View['default'].extend({
     defaultTemplate: topLevelViewTemplate['default'],
@@ -29337,7 +29337,7 @@ enifed('ember-runtime/mixins/enumerable', ['exports', 'ember-metal/core', 'ember
 
     /**
       Returns an array with all of the items in the enumeration where the passed
-      function returns true. This method is the inverse of filter().
+      function returns false. This method is the inverse of filter().
        The callback method you provide should have the following signature (all
       parameters are optional):
        ```javascript
@@ -29346,7 +29346,7 @@ enifed('ember-runtime/mixins/enumerable', ['exports', 'ember-metal/core', 'ember
        - *item* is the current item in the iteration.
       - *index* is the current index in the iteration
       - *enumerable* is the enumerable object itself.
-       It should return the a falsey value to include the item in the results.
+       It should return a falsey value to include the item in the results.
        Note that in addition to a callback, you can also pass an optional target
       object that will be set as "this" on the context. This is a good way
       to give your iterator function access to the current object.
@@ -35690,7 +35690,7 @@ enifed('ember-template-compiler/system/compile_options', ['exports', 'ember-meta
 
     options.buildMeta = function buildMeta(program) {
       return {
-        revision: "Ember@2.0.0-beta.1+canary.23423536",
+        revision: "Ember@2.0.0-beta.1+canary.5b3b67e5",
         loc: program.loc,
         moduleName: options.moduleName
       };
@@ -40215,7 +40215,7 @@ enifed('ember-views/views/container_view', ['exports', 'ember-metal/core', 'embe
 
   'use strict';
 
-  containerViewTemplate['default'].meta.revision = "Ember@2.0.0-beta.1+canary.23423536";
+  containerViewTemplate['default'].meta.revision = "Ember@2.0.0-beta.1+canary.5b3b67e5";
 
   /**
   @module ember
