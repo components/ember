872f6c197f1ff437c5fc30dc6b5b9a248d33c7a6
Tomster <tomster@emberjs.com>
2015-05-15 01:00:05 +0000

    Ember Bower Auto build for https://github.com/emberjs/ember.js/commits/7b335140fd1c1a719eb557506b765432343360a9.

--- ember.debug.js
+++ ember.debug.js
@@ -5,7 +5,7 @@
  *            Portions Copyright 2008-2011 Apple Inc. All rights reserved.
  * @license   Licensed under MIT license
  *            See https://raw.github.com/emberjs/ember.js/master/LICENSE
- * @version   2.0.0-beta.1+canary.5b3b67e5
+ * @version   2.0.0-beta.1+canary.7b335140
  */
 
 (function() {
@@ -8097,7 +8097,7 @@ enifed('ember-htmlbars/keywords/real_outlet', ['exports', 'ember-metal/property_
   @submodule ember-htmlbars
   */
 
-  topLevelViewTemplate['default'].meta.revision = "Ember@2.0.0-beta.1+canary.5b3b67e5";
+  topLevelViewTemplate['default'].meta.revision = "Ember@2.0.0-beta.1+canary.7b335140";
 
   exports['default'] = {
     willRender: function (renderNode, env) {
@@ -10176,7 +10176,7 @@ enifed('ember-htmlbars/templates/select', ['exports', 'ember-template-compiler/s
             dom.insertBoundary(fragment, null);
             return morphs;
           },
-          statements: [["inline", "view", [["get", "view.groupView"]], ["content", ["get", "group.content"], "label", ["get", "group.label"], "selection", ["get", "view.selection"], "multiple", ["get", "view.multiple"], "optionLabelPath", ["get", "view.optionLabelPath"], "optionValuePath", ["get", "view.optionValuePath"], "optionView", ["get", "view.optionView"]]]],
+          statements: [["inline", "view", [["get", "view.groupView"]], ["content", ["get", "group.content"], "label", ["get", "group.label"], "selection", ["get", "view.selection"], "value", ["get", "view.value"], "multiple", ["get", "view.multiple"], "optionLabelPath", ["get", "view.optionLabelPath"], "optionValuePath", ["get", "view.optionValuePath"], "optionView", ["get", "view.optionView"]]]],
           locals: ["group"],
           templates: []
         };
@@ -12997,7 +12997,7 @@ enifed('ember-metal/core', ['exports'], function (exports) {
 
     @class Ember
     @static
-    @version 2.0.0-beta.1+canary.5b3b67e5
+    @version 2.0.0-beta.1+canary.7b335140
   */
 
   if ('undefined' === typeof Ember) {
@@ -13026,10 +13026,10 @@ enifed('ember-metal/core', ['exports'], function (exports) {
   /**
     @property VERSION
     @type String
-    @default '2.0.0-beta.1+canary.5b3b67e5'
+    @default '2.0.0-beta.1+canary.7b335140'
     @static
   */
-  Ember.VERSION = '2.0.0-beta.1+canary.5b3b67e5';
+  Ember.VERSION = '2.0.0-beta.1+canary.7b335140';
 
   /**
     Standard environmental variables. You can define these in a global `EmberENV`
@@ -20380,7 +20380,7 @@ enifed('ember-routing-views/views/link', ['exports', 'ember-metal/core', 'ember-
   @submodule ember-routing-views
   */
 
-  linkToTemplate['default'].meta.revision = "Ember@2.0.0-beta.1+canary.5b3b67e5";
+  linkToTemplate['default'].meta.revision = "Ember@2.0.0-beta.1+canary.7b335140";
 
   var linkViewClassNameBindings = ["active", "loading", "disabled"];
   
@@ -20854,7 +20854,7 @@ enifed('ember-routing-views/views/outlet', ['exports', 'ember-views/views/view',
   @submodule ember-routing-views
   */
 
-  topLevelViewTemplate['default'].meta.revision = "Ember@2.0.0-beta.1+canary.5b3b67e5";
+  topLevelViewTemplate['default'].meta.revision = "Ember@2.0.0-beta.1+canary.7b335140";
 
   var CoreOutletView = View['default'].extend({
     defaultTemplate: topLevelViewTemplate['default'],
@@ -35690,7 +35690,7 @@ enifed('ember-template-compiler/system/compile_options', ['exports', 'ember-meta
 
     options.buildMeta = function buildMeta(program) {
       return {
-        revision: "Ember@2.0.0-beta.1+canary.5b3b67e5",
+        revision: "Ember@2.0.0-beta.1+canary.7b335140",
         loc: program.loc,
         moduleName: options.moduleName
       };
@@ -40215,7 +40215,7 @@ enifed('ember-views/views/container_view', ['exports', 'ember-metal/core', 'embe
 
   'use strict';
 
-  containerViewTemplate['default'].meta.revision = "Ember@2.0.0-beta.1+canary.5b3b67e5";
+  containerViewTemplate['default'].meta.revision = "Ember@2.0.0-beta.1+canary.7b335140";
 
   /**
   @module ember
