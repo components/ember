992f15d797573092065feae4d281498711a7d7d0
Tomster <tomster@emberjs.com>
2015-06-12 03:14:08 +0000

    Ember Bower Auto build for https://github.com/emberjs/ember.js/commits/67682f9d22f314483836eb9d9c60f0035297e8d7.

--- ember-runtime.js
+++ ember-runtime.js
@@ -5,7 +5,7 @@
  *            Portions Copyright 2008-2011 Apple Inc. All rights reserved.
  * @license   Licensed under MIT license
  *            See https://raw.github.com/emberjs/ember.js/master/LICENSE
- * @version   2.0.0-canary+d9730bc9
+ * @version   2.0.0-canary+67682f9d
  */
 
 (function() {
@@ -5005,7 +5005,7 @@ enifed('ember-metal/core', ['exports'], function (exports) {
   
     @class Ember
     @static
-    @version 2.0.0-canary+d9730bc9
+    @version 2.0.0-canary+67682f9d
     @public
   */
 
@@ -5037,11 +5037,11 @@ enifed('ember-metal/core', ['exports'], function (exports) {
   
     @property VERSION
     @type String
-    @default '2.0.0-canary+d9730bc9'
+    @default '2.0.0-canary+67682f9d'
     @static
     @public
   */
-  Ember.VERSION = '2.0.0-canary+d9730bc9';
+  Ember.VERSION = '2.0.0-canary+67682f9d';
 
   /**
     The hash of environment variables used to control various configuration
--- ember-template-compiler.js
+++ ember-template-compiler.js
@@ -5,7 +5,7 @@
  *            Portions Copyright 2008-2011 Apple Inc. All rights reserved.
  * @license   Licensed under MIT license
  *            See https://raw.github.com/emberjs/ember.js/master/LICENSE
- * @version   2.0.0-canary+d9730bc9
+ * @version   2.0.0-canary+67682f9d
  */
 
 (function() {
@@ -4218,7 +4218,7 @@ enifed('ember-metal/core', ['exports'], function (exports) {
   
     @class Ember
     @static
-    @version 2.0.0-canary+d9730bc9
+    @version 2.0.0-canary+67682f9d
     @public
   */
 
@@ -4250,11 +4250,11 @@ enifed('ember-metal/core', ['exports'], function (exports) {
   
     @property VERSION
     @type String
-    @default '2.0.0-canary+d9730bc9'
+    @default '2.0.0-canary+67682f9d'
     @static
     @public
   */
-  Ember.VERSION = '2.0.0-canary+d9730bc9';
+  Ember.VERSION = '2.0.0-canary+67682f9d';
 
   /**
     The hash of environment variables used to control various configuration
@@ -13265,7 +13265,7 @@ enifed("ember-template-compiler/system/compile_options", ["exports", "ember-meta
 
     options.buildMeta = function buildMeta(program) {
       return {
-        revision: "Ember@2.0.0-canary+d9730bc9",
+        revision: "Ember@2.0.0-canary+67682f9d",
         loc: program.loc,
         moduleName: options.moduleName
       };
