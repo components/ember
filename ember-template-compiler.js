d9199a86ae0fc05aedbebf21cd18b91301fe508c
Tomster <tomster@emberjs.com>
2015-06-15 05:58:25 +0000

    Ember Bower Auto build for https://github.com/emberjs/ember.js/commits/bc58a12526fff11b3c0391637abc7fedf7c53282.

--- ember-template-compiler.js
+++ ember-template-compiler.js
@@ -5,7 +5,7 @@
  *            Portions Copyright 2008-2011 Apple Inc. All rights reserved.
  * @license   Licensed under MIT license
  *            See https://raw.github.com/emberjs/ember.js/master/LICENSE
- * @version   2.0.0-beta.1+1a949d03
+ * @version   2.0.0-beta.1+bc58a125
  */
 
 (function() {
@@ -3936,7 +3936,7 @@ enifed('ember-metal/core', ['exports'], function (exports) {
   
     @class Ember
     @static
-    @version 2.0.0-beta.1+1a949d03
+    @version 2.0.0-beta.1+bc58a125
     @public
   */
 
@@ -3968,11 +3968,11 @@ enifed('ember-metal/core', ['exports'], function (exports) {
   
     @property VERSION
     @type String
-    @default '2.0.0-beta.1+1a949d03'
+    @default '2.0.0-beta.1+bc58a125'
     @static
     @public
   */
-  Ember.VERSION = '2.0.0-beta.1+1a949d03';
+  Ember.VERSION = '2.0.0-beta.1+bc58a125';
 
   /**
     The hash of environment variables used to control various configuration
@@ -12810,7 +12810,7 @@ enifed("ember-template-compiler/system/compile_options", ["exports", "ember-meta
 
     options.buildMeta = function buildMeta(program) {
       return {
-        revision: "Ember@2.0.0-beta.1+1a949d03",
+        revision: "Ember@2.0.0-beta.1+bc58a125",
         loc: program.loc,
         moduleName: options.moduleName
       };
