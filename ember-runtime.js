c2f8c8a2ceb945ecb97021174ffe651ccdb55b76
Tomster <tomster@emberjs.com>
2015-06-29 02:25:07 +0000

    Ember Bower Auto build for https://github.com/emberjs/ember.js/commits/f6293824b3a94d11a18e80210f27ee4a89d99cfa.

--- ember-runtime.js
+++ ember-runtime.js
@@ -5,7 +5,7 @@
  *            Portions Copyright 2008-2011 Apple Inc. All rights reserved.
  * @license   Licensed under MIT license
  *            See https://raw.github.com/emberjs/ember.js/master/LICENSE
- * @version   2.0.0-canary+3e5cfdde
+ * @version   2.0.0-canary+f6293824
  */
 
 (function() {
@@ -4776,7 +4776,7 @@ enifed('ember-metal/core', ['exports'], function (exports) {
   
     @class Ember
     @static
-    @version 2.0.0-canary+3e5cfdde
+    @version 2.0.0-canary+f6293824
     @public
   */
 
@@ -4808,11 +4808,11 @@ enifed('ember-metal/core', ['exports'], function (exports) {
   
     @property VERSION
     @type String
-    @default '2.0.0-canary+3e5cfdde'
+    @default '2.0.0-canary+f6293824'
     @static
     @public
   */
-  Ember.VERSION = '2.0.0-canary+3e5cfdde';
+  Ember.VERSION = '2.0.0-canary+f6293824';
 
   /**
     The hash of environment variables used to control various configuration
--- ember.prod.js
+++ ember.prod.js
@@ -5,7 +5,7 @@
  *            Portions Copyright 2008-2011 Apple Inc. All rights reserved.
  * @license   Licensed under MIT license
  *            See https://raw.github.com/emberjs/ember.js/master/LICENSE
- * @version   2.0.0-canary+3e5cfdde
+ * @version   2.0.0-canary+f6293824
  */
 
 (function() {
@@ -8316,7 +8316,7 @@ enifed('ember-htmlbars/keywords/readonly', ['exports', 'ember-htmlbars/keywords/
   }
 });
 enifed('ember-htmlbars/keywords/real_outlet', ['exports', 'ember-metal/core', 'ember-metal/property_get', 'ember-htmlbars/node-managers/view-node-manager', 'ember-htmlbars/templates/top-level-view'], function (exports, _emberMetalCore, _emberMetalProperty_get, _emberHtmlbarsNodeManagersViewNodeManager, _emberHtmlbarsTemplatesTopLevelView) {
-  _emberHtmlbarsTemplatesTopLevelView.default.meta.revision = 'Ember@2.0.0-canary+3e5cfdde';
+  _emberHtmlbarsTemplatesTopLevelView.default.meta.revision = 'Ember@2.0.0-canary+f6293824';
 
   exports.default = {
     willRender: function (renderNode, env) {
@@ -13801,7 +13801,7 @@ enifed('ember-metal/core', ['exports'], function (exports) {
   
     @class Ember
     @static
-    @version 2.0.0-canary+3e5cfdde
+    @version 2.0.0-canary+f6293824
     @public
   */
 
@@ -13833,11 +13833,11 @@ enifed('ember-metal/core', ['exports'], function (exports) {
   
     @property VERSION
     @type String
-    @default '2.0.0-canary+3e5cfdde'
+    @default '2.0.0-canary+f6293824'
     @static
     @public
   */
-  Ember.VERSION = '2.0.0-canary+3e5cfdde';
+  Ember.VERSION = '2.0.0-canary+f6293824';
 
   /**
     The hash of environment variables used to control various configuration
@@ -21822,7 +21822,7 @@ enifed('ember-routing-views', ['exports', 'ember-metal/core', 'ember-metal/featu
 @submodule ember-routing-views
 */
 enifed('ember-routing-views/views/link', ['exports', 'ember-metal/core', 'ember-metal/features', 'ember-metal/property_get', 'ember-metal/property_set', 'ember-metal/computed', 'ember-views/system/utils', 'ember-views/views/component', 'ember-runtime/inject', 'ember-runtime/mixins/controller', 'ember-htmlbars/templates/link-to'], function (exports, _emberMetalCore, _emberMetalFeatures, _emberMetalProperty_get, _emberMetalProperty_set, _emberMetalComputed, _emberViewsSystemUtils, _emberViewsViewsComponent, _emberRuntimeInject, _emberRuntimeMixinsController, _emberHtmlbarsTemplatesLinkTo) {
-  _emberHtmlbarsTemplatesLinkTo.default.meta.revision = 'Ember@2.0.0-canary+3e5cfdde';
+  _emberHtmlbarsTemplatesLinkTo.default.meta.revision = 'Ember@2.0.0-canary+f6293824';
 
   var linkComponentClassNameBindings = ['active', 'loading', 'disabled'];
 
@@ -22330,7 +22330,7 @@ enifed('ember-routing-views/views/link', ['exports', 'ember-metal/core', 'ember-
 
 // FEATURES, Logger, assert
 enifed('ember-routing-views/views/outlet', ['exports', 'ember-views/views/view', 'ember-htmlbars/templates/top-level-view'], function (exports, _emberViewsViewsView, _emberHtmlbarsTemplatesTopLevelView) {
-  _emberHtmlbarsTemplatesTopLevelView.default.meta.revision = 'Ember@2.0.0-canary+3e5cfdde';
+  _emberHtmlbarsTemplatesTopLevelView.default.meta.revision = 'Ember@2.0.0-canary+f6293824';
 
   var CoreOutletView = _emberViewsViewsView.default.extend({
     defaultTemplate: _emberHtmlbarsTemplatesTopLevelView.default,
@@ -24759,7 +24759,7 @@ enifed('ember-routing/system/route', ['exports', 'ember-metal/core', 'ember-meta
       This hook is executed when the router enters the route. It is not executed
       when the model for the route changes.
        @method activate
-      @private
+      @public
     */
     activate: K,
 
@@ -37235,7 +37235,7 @@ enifed('ember-template-compiler/system/compile_options', ['exports', 'ember-meta
 
     options.buildMeta = function buildMeta(program) {
       return {
-        revision: 'Ember@2.0.0-canary+3e5cfdde',
+        revision: 'Ember@2.0.0-canary+f6293824',
         loc: program.loc,
         moduleName: options.moduleName
       };
@@ -41140,7 +41140,7 @@ enifed('ember-views/views/component', ['exports', 'ember-metal/core', 'ember-vie
 });
 // Ember.assert, Ember.Handlebars
 enifed('ember-views/views/container_view', ['exports', 'ember-metal/core', 'ember-runtime/mixins/mutable_array', 'ember-views/views/view', 'ember-metal/property_get', 'ember-metal/property_set', 'ember-metal/mixin', 'ember-metal/events', 'ember-htmlbars/templates/container-view'], function (exports, _emberMetalCore, _emberRuntimeMixinsMutable_array, _emberViewsViewsView, _emberMetalProperty_get, _emberMetalProperty_set, _emberMetalMixin, _emberMetalEvents, _emberHtmlbarsTemplatesContainerView) {
-  _emberHtmlbarsTemplatesContainerView.default.meta.revision = 'Ember@2.0.0-canary+3e5cfdde';
+  _emberHtmlbarsTemplatesContainerView.default.meta.revision = 'Ember@2.0.0-canary+f6293824';
 
   /**
   @module ember
