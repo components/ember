aa5238dddb13feea48caf8eb1753887d66d9b234
Tomster <tomster@emberjs.com>
2015-07-08 05:19:47 +0000

    Ember Bower Auto build for https://github.com/emberjs/ember.js/commits/6276f931b2ada79a6c5ddc3ff3796c34c008608f.

--- ember-runtime.js
+++ ember-runtime.js
@@ -5,7 +5,7 @@
  *            Portions Copyright 2008-2011 Apple Inc. All rights reserved.
  * @license   Licensed under MIT license
  *            See https://raw.github.com/emberjs/ember.js/master/LICENSE
- * @version   2.0.0-canary+663aff3f
+ * @version   2.0.0-canary+6276f931
  */
 
 (function() {
@@ -4765,7 +4765,7 @@ enifed('ember-metal/core', ['exports'], function (exports) {
   
     @class Ember
     @static
-    @version 2.0.0-canary+663aff3f
+    @version 2.0.0-canary+6276f931
     @public
   */
 
@@ -4797,11 +4797,11 @@ enifed('ember-metal/core', ['exports'], function (exports) {
   
     @property VERSION
     @type String
-    @default '2.0.0-canary+663aff3f'
+    @default '2.0.0-canary+6276f931'
     @static
     @public
   */
-  Ember.VERSION = '2.0.0-canary+663aff3f';
+  Ember.VERSION = '2.0.0-canary+6276f931';
 
   /**
     The hash of environment variables used to control various configuration
@@ -14999,7 +14999,7 @@ enifed('ember-runtime/mixins/enumerable', ['exports', 'ember-metal/core', 'ember
       @param {Function} callback The callback to execute
       @param {Object} [target] The target object to use
       @return {Array} A filtered array.
-      @public
+      @private
     */
     filter: function (callback, target) {
       var ret = _emberMetalCore.default.A();
