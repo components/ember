94b168b6534131ca5200cf47c7816fb149a5374a
Tomster <tomster@emberjs.com>
2015-07-08 05:07:03 +0000

    Ember Bower Auto build for https://github.com/emberjs/ember.js/commits/6276f931b2ada79a6c5ddc3ff3796c34c008608f.

--- ember-runtime.js
+++ ember-runtime.js
@@ -5,7 +5,7 @@
  *            Portions Copyright 2008-2011 Apple Inc. All rights reserved.
  * @license   Licensed under MIT license
  *            See https://raw.github.com/emberjs/ember.js/master/LICENSE
- * @version   2.0.0-canary+14536c1e
+ * @version   2.0.0-canary+6276f931
  */
 
 (function() {
@@ -4765,7 +4765,7 @@ enifed('ember-metal/core', ['exports'], function (exports) {
   
     @class Ember
     @static
-    @version 2.0.0-canary+14536c1e
+    @version 2.0.0-canary+6276f931
     @public
   */
 
@@ -4797,11 +4797,11 @@ enifed('ember-metal/core', ['exports'], function (exports) {
   
     @property VERSION
     @type String
-    @default '2.0.0-canary+14536c1e'
+    @default '2.0.0-canary+6276f931'
     @static
     @public
   */
-  Ember.VERSION = '2.0.0-canary+14536c1e';
+  Ember.VERSION = '2.0.0-canary+6276f931';
 
   /**
     The hash of environment variables used to control various configuration
@@ -15062,7 +15062,7 @@ enifed('ember-runtime/mixins/enumerable', ['exports', 'ember-metal/core', 'ember
       @param {String} key the property to test
       @param {String} [value] optional value to test against.
       @return {Array} rejected array
-      @private
+      @public
     */
     rejectBy: function (key, value) {
       var exactValue = function (item) {
