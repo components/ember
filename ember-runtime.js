347c1b36c4ebf2e35f7cf724007695b1e39631e5
Tomster <tomster@emberjs.com>
2015-07-08 04:21:29 +0000

    Ember Bower Auto build for https://github.com/emberjs/ember.js/commits/4ff7ae35d93620274eea53ba2365f53a8ee2fad0.

--- ember-runtime.js
+++ ember-runtime.js
@@ -5,7 +5,7 @@
  *            Portions Copyright 2008-2011 Apple Inc. All rights reserved.
  * @license   Licensed under MIT license
  *            See https://raw.github.com/emberjs/ember.js/master/LICENSE
- * @version   2.0.0-canary+e748d68c
+ * @version   2.0.0-canary+4ff7ae35
  */
 
 (function() {
@@ -4765,7 +4765,7 @@ enifed('ember-metal/core', ['exports'], function (exports) {
   
     @class Ember
     @static
-    @version 2.0.0-canary+e748d68c
+    @version 2.0.0-canary+4ff7ae35
     @public
   */
 
@@ -4797,11 +4797,11 @@ enifed('ember-metal/core', ['exports'], function (exports) {
   
     @property VERSION
     @type String
-    @default '2.0.0-canary+e748d68c'
+    @default '2.0.0-canary+4ff7ae35'
     @static
     @public
   */
-  Ember.VERSION = '2.0.0-canary+e748d68c';
+  Ember.VERSION = '2.0.0-canary+4ff7ae35';
 
   /**
     The hash of environment variables used to control various configuration
@@ -14925,7 +14925,7 @@ enifed('ember-runtime/mixins/enumerable', ['exports', 'ember-metal/core', 'ember
       @param {String} key The key to set
       @param {Object} value The object to set
       @return {Object} receiver
-      @public
+      @private
     */
     setEach: function (key, value) {
       return this.forEach(function (item) {
--- ember-testing.js
+++ ember-testing.js
@@ -5,7 +5,7 @@
  *            Portions Copyright 2008-2011 Apple Inc. All rights reserved.
  * @license   Licensed under MIT license
  *            See https://raw.github.com/emberjs/ember.js/master/LICENSE
- * @version   2.0.0-canary+e748d68c
+ * @version   2.0.0-canary+4ff7ae35
  */
 
 (function() {
