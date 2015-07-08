53f149f55f700ae73d96dc24fc03c21825c5ef0a
Tomster <tomster@emberjs.com>
2015-07-08 03:59:12 +0000

    Ember Bower Auto build for https://github.com/emberjs/ember.js/commits/283f5df0adde87e5f8e3404787eebe08aa697f0b.

--- ember-tests.prod.js
+++ ember-tests.prod.js
@@ -5,7 +5,7 @@
  *            Portions Copyright 2008-2011 Apple Inc. All rights reserved.
  * @license   Licensed under MIT license
  *            See https://raw.github.com/emberjs/ember.js/master/LICENSE
- * @version   2.0.0-canary+de0dc73c
+ * @version   2.0.0-canary+283f5df0
  */
 
 (function() {
@@ -45573,7 +45573,7 @@ enifed('ember-template-compiler/tests/system/compile_test', ['exports', 'ember-t
 
     var actual = _emberTemplateCompilerSystemCompile.default(templateString);
 
-    equal(actual.meta.revision, 'Ember@2.0.0-canary+de0dc73c', 'revision is included in generated template');
+    equal(actual.meta.revision, 'Ember@2.0.0-canary+283f5df0', 'revision is included in generated template');
   });
 
   QUnit.test('the template revision is different than the HTMLBars default revision', function () {
