df8e16aec5cc0e6e4d0cb14ab5a06074c31a6b50
Tomster <tomster@emberjs.com>
2015-07-24 11:18:51 +0000

    Ember Bower Auto build for https://github.com/emberjs/ember.js/commits/e09c61a717c1c34b03d4cbe30fe87ba4dae9499c.

--- ember-runtime.js
+++ ember-runtime.js
@@ -5,7 +5,7 @@
  *            Portions Copyright 2008-2011 Apple Inc. All rights reserved.
  * @license   Licensed under MIT license
  *            See https://raw.github.com/emberjs/ember.js/master/LICENSE
- * @version   2.0.0-canary+eeae38e9
+ * @version   2.0.0-canary+e09c61a7
  */
 
 (function() {
@@ -4754,7 +4754,7 @@ enifed('ember-metal/core', ['exports'], function (exports) {
   
     @class Ember
     @static
-    @version 2.0.0-canary+eeae38e9
+    @version 2.0.0-canary+e09c61a7
     @public
   */
 
@@ -4786,11 +4786,11 @@ enifed('ember-metal/core', ['exports'], function (exports) {
   
     @property VERSION
     @type String
-    @default '2.0.0-canary+eeae38e9'
+    @default '2.0.0-canary+e09c61a7'
     @static
     @public
   */
-  Ember.VERSION = '2.0.0-canary+eeae38e9';
+  Ember.VERSION = '2.0.0-canary+e09c61a7';
 
   /**
     The hash of environment variables used to control various configuration
--- ember-tests.prod.js
+++ ember-tests.prod.js
@@ -5,7 +5,7 @@
  *            Portions Copyright 2008-2011 Apple Inc. All rights reserved.
  * @license   Licensed under MIT license
  *            See https://raw.github.com/emberjs/ember.js/master/LICENSE
- * @version   2.0.0-canary+eeae38e9
+ * @version   2.0.0-canary+e09c61a7
  */
 
 (function() {
@@ -42330,7 +42330,7 @@ enifed('ember-template-compiler/tests/system/compile_test', ['exports', 'ember-t
 
     var actual = _emberTemplateCompilerSystemCompile.default(templateString);
 
-    equal(actual.meta.revision, 'Ember@2.0.0-canary+eeae38e9', 'revision is included in generated template');
+    equal(actual.meta.revision, 'Ember@2.0.0-canary+e09c61a7', 'revision is included in generated template');
   });
 
   QUnit.test('the template revision is different than the HTMLBars default revision', function () {
@@ -58742,7 +58742,7 @@ enifed('ember/tests/routing/query_params_test', ['exports', 'ember', 'ember-meta
     }, 'You passed in `[{"commitBy":{"replace":true}}]` as the value for `queryParams` but `queryParams` cannot be an Array');
   });
 
-  QUnit.test('handle routes names that clash with Object.prototype properties', function () {
+  QUnit.test('handle routes names that class with Object.prototype properties', function () {
     expect(1);
 
     Router.map(function () {
