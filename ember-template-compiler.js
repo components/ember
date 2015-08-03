d9077f1d23625c5ebe7d9c028255cd4e4f977264
Tomster <tomster@emberjs.com>
2015-08-03 02:44:18 +0000

    Ember Bower Auto build for https://github.com/emberjs/ember.js/commits/e1c80a4f3d8376bbd7aef571e9252d2b5a208483.

--- ember-template-compiler.js
+++ ember-template-compiler.js
@@ -5,7 +5,7 @@
  *            Portions Copyright 2008-2011 Apple Inc. All rights reserved.
  * @license   Licensed under MIT license
  *            See https://raw.github.com/emberjs/ember.js/master/LICENSE
- * @version   2.0.0-canary+6c0dce94
+ * @version   2.0.0-canary+e1c80a4f
  */
 
 (function() {
@@ -4245,7 +4245,7 @@ enifed('ember-metal/core', ['exports'], function (exports) {
   
     @class Ember
     @static
-    @version 2.0.0-canary+6c0dce94
+    @version 2.0.0-canary+e1c80a4f
     @public
   */
 
@@ -4279,11 +4279,11 @@ enifed('ember-metal/core', ['exports'], function (exports) {
   
     @property VERSION
     @type String
-    @default '2.0.0-canary+6c0dce94'
+    @default '2.0.0-canary+e1c80a4f'
     @static
     @public
   */
-  Ember.VERSION = '2.0.0-canary+6c0dce94';
+  Ember.VERSION = '2.0.0-canary+e1c80a4f';
 
   /**
     The hash of environment variables used to control various configuration
@@ -12210,7 +12210,7 @@ enifed('ember-template-compiler/system/compile_options', ['exports', 'ember-meta
     options.buildMeta = function buildMeta(program) {
       return {
         topLevel: detectTopLevel(program),
-        revision: 'Ember@2.0.0-canary+6c0dce94',
+        revision: 'Ember@2.0.0-canary+e1c80a4f',
         loc: program.loc,
         moduleName: options.moduleName
       };
--- ember.prod.js
+++ ember.prod.js
@@ -5,7 +5,7 @@
  *            Portions Copyright 2008-2011 Apple Inc. All rights reserved.
  * @license   Licensed under MIT license
  *            See https://raw.github.com/emberjs/ember.js/master/LICENSE
- * @version   2.0.0-canary+6c0dce94
+ * @version   2.0.0-canary+e1c80a4f
  */
 
 (function() {
@@ -8219,7 +8219,7 @@ enifed('ember-htmlbars/keywords/outlet', ['exports', 'ember-metal/core', 'ember-
 
   'use strict';
 
-  _emberHtmlbarsTemplatesTopLevelView.default.meta.revision = 'Ember@2.0.0-canary+6c0dce94';
+  _emberHtmlbarsTemplatesTopLevelView.default.meta.revision = 'Ember@2.0.0-canary+e1c80a4f';
 
   exports.default = {
     willRender: function (renderNode, env) {
@@ -13938,7 +13938,7 @@ enifed('ember-metal/core', ['exports'], function (exports) {
   
     @class Ember
     @static
-    @version 2.0.0-canary+6c0dce94
+    @version 2.0.0-canary+e1c80a4f
     @public
   */
 
@@ -13972,11 +13972,11 @@ enifed('ember-metal/core', ['exports'], function (exports) {
   
     @property VERSION
     @type String
-    @default '2.0.0-canary+6c0dce94'
+    @default '2.0.0-canary+e1c80a4f'
     @static
     @public
   */
-  Ember.VERSION = '2.0.0-canary+6c0dce94';
+  Ember.VERSION = '2.0.0-canary+e1c80a4f';
 
   /**
     The hash of environment variables used to control various configuration
@@ -21853,7 +21853,7 @@ enifed('ember-routing-views/views/link', ['exports', 'ember-metal/core', 'ember-
 
   'use strict';
 
-  _emberHtmlbarsTemplatesLinkTo.default.meta.revision = 'Ember@2.0.0-canary+6c0dce94';
+  _emberHtmlbarsTemplatesLinkTo.default.meta.revision = 'Ember@2.0.0-canary+e1c80a4f';
 
   var linkComponentClassNameBindings = ['active', 'loading', 'disabled'];
 
@@ -22351,7 +22351,7 @@ enifed('ember-routing-views/views/outlet', ['exports', 'ember-views/views/view',
 
   'use strict';
 
-  _emberHtmlbarsTemplatesTopLevelView.default.meta.revision = 'Ember@2.0.0-canary+6c0dce94';
+  _emberHtmlbarsTemplatesTopLevelView.default.meta.revision = 'Ember@2.0.0-canary+e1c80a4f';
 
   var CoreOutletView = _emberViewsViewsView.default.extend({
     defaultTemplate: _emberHtmlbarsTemplatesTopLevelView.default,
@@ -36518,7 +36518,7 @@ enifed('ember-template-compiler/system/compile_options', ['exports', 'ember-meta
     options.buildMeta = function buildMeta(program) {
       return {
         topLevel: detectTopLevel(program),
-        revision: 'Ember@2.0.0-canary+6c0dce94',
+        revision: 'Ember@2.0.0-canary+e1c80a4f',
         loc: program.loc,
         moduleName: options.moduleName
       };
@@ -39932,7 +39932,7 @@ enifed('ember-views/views/component', ['exports', 'ember-metal/core', 'ember-vie
 enifed('ember-views/views/container_view', ['exports', 'ember-metal/core', 'ember-runtime/mixins/mutable_array', 'ember-views/views/view', 'ember-metal/property_get', 'ember-metal/property_set', 'ember-metal/mixin', 'ember-metal/events', 'ember-htmlbars/templates/container-view'], function (exports, _emberMetalCore, _emberRuntimeMixinsMutable_array, _emberViewsViewsView, _emberMetalProperty_get, _emberMetalProperty_set, _emberMetalMixin, _emberMetalEvents, _emberHtmlbarsTemplatesContainerView) {
   'use strict';
 
-  _emberHtmlbarsTemplatesContainerView.default.meta.revision = 'Ember@2.0.0-canary+6c0dce94';
+  _emberHtmlbarsTemplatesContainerView.default.meta.revision = 'Ember@2.0.0-canary+e1c80a4f';
 
   /**
   @module ember
