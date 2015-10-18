094e946135b889774476c30f712fd86a5adfc470
Tomster <tomster@emberjs.com>
2015-10-18 21:36:03 +0000

    Ember Bower Auto build for https://github.com/emberjs/ember.js/commits/72cdc4a636e5605cfda7883eeff7122811187630.

--- ember-template-compiler.js
+++ ember-template-compiler.js
@@ -6,7 +6,7 @@
  *            Portions Copyright 2008-2011 Apple Inc. All rights reserved.
  * @license   Licensed under MIT license
  *            See https://raw.github.com/emberjs/ember.js/master/LICENSE
- * @version   2.3.0-canary+bb5cc203
+ * @version   2.3.0-canary+72cdc4a6
  */
 
 var enifed, requireModule, require, requirejs, Ember;
@@ -4138,7 +4138,7 @@ enifed('ember-metal/core', ['exports'], function (exports) {
   
     @class Ember
     @static
-    @version 2.3.0-canary+bb5cc203
+    @version 2.3.0-canary+72cdc4a6
     @public
   */
 
@@ -4182,11 +4182,11 @@ enifed('ember-metal/core', ['exports'], function (exports) {
   
     @property VERSION
     @type String
-    @default '2.3.0-canary+bb5cc203'
+    @default '2.3.0-canary+72cdc4a6'
     @static
     @public
   */
-  Ember.VERSION = '2.3.0-canary+bb5cc203';
+  Ember.VERSION = '2.3.0-canary+72cdc4a6';
 
   /**
     The hash of environment variables used to control various configuration
@@ -5502,7 +5502,7 @@ enifed("ember-metal/is_none", ["exports"], function (exports) {
     Ember.isNone(undefined);     // true
     Ember.isNone('');            // false
     Ember.isNone([]);            // false
-    Ember.isNone(function() {});  // false
+    Ember.isNone(function() {}); // false
     ```
   
     @method isNone
@@ -12575,7 +12575,7 @@ enifed('ember-template-compiler/system/compile_options', ['exports', 'ember-meta
     options.buildMeta = function buildMeta(program) {
       return {
         fragmentReason: fragmentReason(program),
-        revision: 'Ember@2.3.0-canary+bb5cc203',
+        revision: 'Ember@2.3.0-canary+72cdc4a6',
         loc: program.loc,
         moduleName: options.moduleName
       };
--- ember-testing.js
+++ ember-testing.js
@@ -6,7 +6,7 @@
  *            Portions Copyright 2008-2011 Apple Inc. All rights reserved.
  * @license   Licensed under MIT license
  *            See https://raw.github.com/emberjs/ember.js/master/LICENSE
- * @version   2.3.0-canary+bb5cc203
+ * @version   2.3.0-canary+72cdc4a6
  */
 
 var enifed, requireModule, require, requirejs, Ember;
