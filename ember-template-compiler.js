a640c55f61a052b802087dceb883c0d71541b7ab
Tomster <tomster@emberjs.com>
2015-11-11 15:26:44 +0000

    Ember Bower Auto build for https://github.com/emberjs/ember.js/commits/44e5f282dd2084a39d510bca5e7e2f8a5f776bb6.

--- ember-template-compiler.js
+++ ember-template-compiler.js
@@ -6,7 +6,7 @@
  *            Portions Copyright 2008-2011 Apple Inc. All rights reserved.
  * @license   Licensed under MIT license
  *            See https://raw.github.com/emberjs/ember.js/master/LICENSE
- * @version   2.3.0-canary+f1b40368
+ * @version   2.3.0-canary+44e5f282
  */
 
 var enifed, requireModule, require, requirejs, Ember;
@@ -4140,7 +4140,7 @@ enifed('ember-metal/core', ['exports'], function (exports) {
   
     @class Ember
     @static
-    @version 2.3.0-canary+f1b40368
+    @version 2.3.0-canary+44e5f282
     @public
   */
 
@@ -4184,11 +4184,11 @@ enifed('ember-metal/core', ['exports'], function (exports) {
   
     @property VERSION
     @type String
-    @default '2.3.0-canary+f1b40368'
+    @default '2.3.0-canary+44e5f282'
     @static
     @public
   */
-  Ember.VERSION = '2.3.0-canary+f1b40368';
+  Ember.VERSION = '2.3.0-canary+44e5f282';
 
   /**
     The hash of environment variables used to control various configuration
@@ -12600,7 +12600,7 @@ enifed('ember-template-compiler/system/compile_options', ['exports', 'ember-meta
     options.buildMeta = function buildMeta(program) {
       return {
         fragmentReason: fragmentReason(program),
-        revision: 'Ember@2.3.0-canary+f1b40368',
+        revision: 'Ember@2.3.0-canary+44e5f282',
         loc: program.loc,
         moduleName: options.moduleName
       };
--- ember-testing.js
+++ ember-testing.js
@@ -6,7 +6,7 @@
  *            Portions Copyright 2008-2011 Apple Inc. All rights reserved.
  * @license   Licensed under MIT license
  *            See https://raw.github.com/emberjs/ember.js/master/LICENSE
- * @version   2.3.0-canary+f1b40368
+ * @version   2.3.0-canary+44e5f282
  */
 
 var enifed, requireModule, require, requirejs, Ember;
