2d80298845cbbca527a12d16c3cef4af84054d45
Tomster <tomster@emberjs.com>
2015-11-20 04:20:56 +0000

    Ember Bower Auto build for https://github.com/emberjs/ember.js/commits/a4a44030146e67721efba5ebd902a69ce3ada1f6.

--- ember-runtime.js
+++ ember-runtime.js
@@ -6,7 +6,7 @@
  *            Portions Copyright 2008-2011 Apple Inc. All rights reserved.
  * @license   Licensed under MIT license
  *            See https://raw.github.com/emberjs/ember.js/master/LICENSE
- * @version   2.3.0-beta.1+d4bd779a
+ * @version   2.3.0-beta.1+a4a44030
  */
 
 var enifed, requireModule, require, requirejs, Ember;
@@ -1330,6 +1330,19 @@ enifed('container/container', ['exports', 'ember-metal/core', 'ember-metal/debug
       } else {
         resetCache(this);
       }
+    },
+
+    /**
+     Returns an object that can be used to provide an owner to a
+     manually created instance.
+      @private
+     @method ownerInjection
+     @returns { Object }
+    */
+    ownerInjection: function () {
+      var _ref;
+
+      return _ref = {}, _ref[_containerOwner.OWNER] = this.owner, _ref;
     }
   };
 
@@ -4730,7 +4743,7 @@ enifed('ember-metal/core', ['exports'], function (exports) {
   
     @class Ember
     @static
-    @version 2.3.0-beta.1+d4bd779a
+    @version 2.3.0-beta.1+a4a44030
     @public
   */
 
@@ -4774,11 +4787,11 @@ enifed('ember-metal/core', ['exports'], function (exports) {
   
     @property VERSION
     @type String
-    @default '2.3.0-beta.1+d4bd779a'
+    @default '2.3.0-beta.1+a4a44030'
     @static
     @public
   */
-  Ember.VERSION = '2.3.0-beta.1+d4bd779a';
+  Ember.VERSION = '2.3.0-beta.1+a4a44030';
 
   /**
     The hash of environment variables used to control various configuration
@@ -14456,6 +14469,23 @@ enifed('ember-runtime/mixins/container_proxy', ['exports', 'ember-metal/run_loop
      @property {Ember.Container} __container__
      */
     __container__: null,
+
+    /**
+     Returns an object that can be used to provide an owner to a
+     manually created instance.
+      Example:
+      ```
+     let owner = Ember.getOwner(this);
+      User.create(
+       owner.ownerInjection(),
+       { username: 'rwjblue' }
+     )
+     ```
+      @public
+     @method ownerInjection
+     @return {Object}
+    */
+    ownerInjection: containerAlias('ownerInjection'),
 
     /**
      Given a fullName return a corresponding instance.
--- ember-template-compiler.js
+++ ember-template-compiler.js
@@ -6,7 +6,7 @@
  *            Portions Copyright 2008-2011 Apple Inc. All rights reserved.
  * @license   Licensed under MIT license
  *            See https://raw.github.com/emberjs/ember.js/master/LICENSE
- * @version   2.3.0-beta.1+d4bd779a
+ * @version   2.3.0-beta.1+a4a44030
  */
 
 var enifed, requireModule, require, requirejs, Ember;
@@ -4130,7 +4130,7 @@ enifed('ember-metal/core', ['exports'], function (exports) {
   
     @class Ember
     @static
-    @version 2.3.0-beta.1+d4bd779a
+    @version 2.3.0-beta.1+a4a44030
     @public
   */
 
@@ -4174,11 +4174,11 @@ enifed('ember-metal/core', ['exports'], function (exports) {
   
     @property VERSION
     @type String
-    @default '2.3.0-beta.1+d4bd779a'
+    @default '2.3.0-beta.1+a4a44030'
     @static
     @public
   */
-  Ember.VERSION = '2.3.0-beta.1+d4bd779a';
+  Ember.VERSION = '2.3.0-beta.1+a4a44030';
 
   /**
     The hash of environment variables used to control various configuration
@@ -12579,7 +12579,7 @@ enifed('ember-template-compiler/system/compile_options', ['exports', 'ember-meta
     options.buildMeta = function buildMeta(program) {
       return {
         fragmentReason: fragmentReason(program),
-        revision: 'Ember@2.3.0-beta.1+d4bd779a',
+        revision: 'Ember@2.3.0-beta.1+a4a44030',
         loc: program.loc,
         moduleName: options.moduleName
       };
