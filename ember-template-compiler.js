7cd405921254b08829e9450ddf7f72a0a7a19312
Tomster <tomster@emberjs.com>
2015-12-12 13:55:57 +0000

    Ember Bower Auto build for https://github.com/emberjs/ember.js/commits/302247a0c6212adc5acfbfcf541f36f230b4c8ed.

--- ember-template-compiler.js
+++ ember-template-compiler.js
@@ -6,7 +6,7 @@
  *            Portions Copyright 2008-2011 Apple Inc. All rights reserved.
  * @license   Licensed under MIT license
  *            See https://raw.github.com/emberjs/ember.js/master/LICENSE
- * @version   2.4.0-canary+039167ce
+ * @version   2.4.0-canary+302247a0
  */
 
 var enifed, requireModule, require, requirejs, Ember;
@@ -4149,7 +4149,7 @@ enifed('ember-metal/core', ['exports', 'require'], function (exports, _require) 
   
     @class Ember
     @static
-    @version 2.4.0-canary+039167ce
+    @version 2.4.0-canary+302247a0
     @public
   */
 
@@ -4191,11 +4191,11 @@ enifed('ember-metal/core', ['exports', 'require'], function (exports, _require) 
   
     @property VERSION
     @type String
-    @default '2.4.0-canary+039167ce'
+    @default '2.4.0-canary+302247a0'
     @static
     @public
   */
-  Ember.VERSION = '2.4.0-canary+039167ce';
+  Ember.VERSION = '2.4.0-canary+302247a0';
 
   /**
     The hash of environment variables used to control various configuration
@@ -12628,7 +12628,7 @@ enifed('ember-template-compiler/system/compile_options', ['exports', 'ember-meta
     options.buildMeta = function buildMeta(program) {
       return {
         fragmentReason: fragmentReason(program),
-        revision: 'Ember@2.4.0-canary+039167ce',
+        revision: 'Ember@2.4.0-canary+302247a0',
         loc: program.loc,
         moduleName: options.moduleName
       };
@@ -18155,7 +18155,7 @@ enifed("htmlbars-syntax/parser/tokenizer-event-handlers", ["exports", "htmlbars-
       element.loc.end.line = this.tokenizer.line;
       element.loc.end.column = this.tokenizer.column;
 
-      if (disableComponentGeneration || element.tag.indexOf("-") === -1) {
+      if (disableComponentGeneration || cannotBeComponent(element.tag)) {
         _htmlbarsSyntaxUtils.appendChild(parent, element);
       } else {
         var program = _htmlbarsSyntaxBuilders.default.program(element.children);
@@ -18254,6 +18254,10 @@ enifed("htmlbars-syntax/parser/tokenizer-event-handlers", ["exports", "htmlbars-
     }
 
     return _htmlbarsSyntaxBuilders.default.concat(parts);
+  }
+
+  function cannotBeComponent(tagName) {
+    return tagName.indexOf("-") === -1 && tagName.indexOf(".") === -1;
   }
 
   function validateStartTag(tag, tokenizer) {
