6951291a2c9b735876edb5f4ab47b1b26c615f7f
Tomster <tomster@emberjs.com>
2015-12-28 00:18:23 +0000

    Ember Bower Auto build for https://github.com/emberjs/ember.js/commits/7b51fbb553a95bbc215ec15fe2ffe190ed6864c5.

--- ember-runtime.js
+++ ember-runtime.js
@@ -6,7 +6,7 @@
  *            Portions Copyright 2008-2011 Apple Inc. All rights reserved.
  * @license   Licensed under MIT license
  *            See https://raw.github.com/emberjs/ember.js/master/LICENSE
- * @version   2.4.0-canary+850a1c84
+ * @version   2.4.0-canary+7b51fbb5
  */
 
 var enifed, requireModule, require, requirejs, Ember;
@@ -4919,7 +4919,7 @@ enifed('ember-metal/core', ['exports', 'require'], function (exports, _require) 
   
     @class Ember
     @static
-    @version 2.4.0-canary+850a1c84
+    @version 2.4.0-canary+7b51fbb5
     @public
   */
 
@@ -4961,11 +4961,11 @@ enifed('ember-metal/core', ['exports', 'require'], function (exports, _require) 
   
     @property VERSION
     @type String
-    @default '2.4.0-canary+850a1c84'
+    @default '2.4.0-canary+7b51fbb5'
     @static
     @public
   */
-  Ember.VERSION = '2.4.0-canary+850a1c84';
+  Ember.VERSION = '2.4.0-canary+7b51fbb5';
 
   /**
     The hash of environment variables used to control various configuration
--- ember-tests.js
+++ ember-tests.js
@@ -6,7 +6,7 @@
  *            Portions Copyright 2008-2011 Apple Inc. All rights reserved.
  * @license   Licensed under MIT license
  *            See https://raw.github.com/emberjs/ember.js/master/LICENSE
- * @version   2.4.0-canary+850a1c84
+ * @version   2.4.0-canary+7b51fbb5
  */
 
 var enifed, requireModule, require, requirejs, Ember;
@@ -53134,7 +53134,7 @@ enifed('ember-template-compiler/tests/system/compile_test', ['exports', 'ember-t
 
     var actual = _emberTemplateCompilerSystemCompile.default(templateString);
 
-    equal(actual.meta.revision, 'Ember@2.4.0-canary+850a1c84', 'revision is included in generated template');
+    equal(actual.meta.revision, 'Ember@2.4.0-canary+7b51fbb5', 'revision is included in generated template');
   });
 
   QUnit.test('the template revision is different than the HTMLBars default revision', function () {
