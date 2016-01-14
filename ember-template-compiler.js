71dc1965cedeb6300ff776eabc49f75e01bf2152
Robert Jackson <robert.w.jackson@me.com>
2016-01-14 12:58:31 -0800

    Release v2.2.1.

--- ember-template-compiler.js
+++ ember-template-compiler.js
@@ -6,7 +6,7 @@
  *            Portions Copyright 2008-2011 Apple Inc. All rights reserved.
  * @license   Licensed under MIT license
  *            See https://raw.github.com/emberjs/ember.js/master/LICENSE
- * @version   2.2.0
+ * @version   2.2.1
  */
 
 var enifed, requireModule, require, requirejs, Ember;
@@ -4136,7 +4136,7 @@ enifed('ember-metal/core', ['exports'], function (exports) {
   
     @class Ember
     @static
-    @version 2.2.0
+    @version 2.2.1
     @public
   */
 
@@ -4180,11 +4180,11 @@ enifed('ember-metal/core', ['exports'], function (exports) {
   
     @property VERSION
     @type String
-    @default '2.2.0'
+    @default '2.2.1'
     @static
     @public
   */
-  Ember.VERSION = '2.2.0';
+  Ember.VERSION = '2.2.1';
 
   /**
     The hash of environment variables used to control various configuration
@@ -12515,7 +12515,7 @@ enifed('ember-template-compiler/system/compile_options', ['exports', 'ember-meta
     options.buildMeta = function buildMeta(program) {
       return {
         fragmentReason: fragmentReason(program),
-        revision: 'Ember@2.2.0',
+        revision: 'Ember@2.2.1',
         loc: program.loc,
         moduleName: options.moduleName
       };
@@ -19592,8 +19592,8 @@ enifed('morph-range', ['exports', 'morph-range/utils'], function (exports, _morp
           return this.setNode(content);
         }
         /* Handlebars.SafeString */
-        if (typeof content.string === 'string') {
-          return this.setHTML(content.string);
+        if (typeof content.toHTML === 'function') {
+          return this.setHTML(content.toHTML());
         }
         if (this.parseTextAsHTML) {
           return this.setHTML(content.toString());
--- ember.debug.js
+++ ember.debug.js
@@ -6,7 +6,7 @@
  *            Portions Copyright 2008-2011 Apple Inc. All rights reserved.
  * @license   Licensed under MIT license
  *            See https://raw.github.com/emberjs/ember.js/master/LICENSE
- * @version   2.2.0
+ * @version   2.2.1
  */
 
 var enifed, requireModule, require, requirejs, Ember;
@@ -9738,7 +9738,7 @@ enifed('ember-htmlbars/keywords/outlet', ['exports', 'ember-metal/debug', 'ember
 
   'use strict';
 
-  _emberHtmlbarsTemplatesTopLevelView.default.meta.revision = 'Ember@2.2.0';
+  _emberHtmlbarsTemplatesTopLevelView.default.meta.revision = 'Ember@2.2.1';
 
   /**
     The `{{outlet}}` helper lets you specify where a child routes will render in
@@ -15427,7 +15427,7 @@ enifed('ember-metal/core', ['exports'], function (exports) {
   
     @class Ember
     @static
-    @version 2.2.0
+    @version 2.2.1
     @public
   */
 
@@ -15471,11 +15471,11 @@ enifed('ember-metal/core', ['exports'], function (exports) {
   
     @property VERSION
     @type String
-    @default '2.2.0'
+    @default '2.2.1'
     @static
     @public
   */
-  Ember.VERSION = '2.2.0';
+  Ember.VERSION = '2.2.1';
 
   /**
     The hash of environment variables used to control various configuration
@@ -29125,7 +29125,7 @@ enifed('ember-routing-views/components/link-to', ['exports', 'ember-metal/logger
 
   'use strict';
 
-  _emberHtmlbarsTemplatesLinkTo.default.meta.revision = 'Ember@2.2.0';
+  _emberHtmlbarsTemplatesLinkTo.default.meta.revision = 'Ember@2.2.1';
 
   /**
     `Ember.LinkComponent` renders an element whose `click` event triggers a
@@ -29615,7 +29615,7 @@ enifed('ember-routing-views/views/outlet', ['exports', 'ember-views/views/view',
 
   'use strict';
 
-  _emberHtmlbarsTemplatesTopLevelView.default.meta.revision = 'Ember@2.2.0';
+  _emberHtmlbarsTemplatesTopLevelView.default.meta.revision = 'Ember@2.2.1';
 
   var CoreOutletView = _emberViewsViewsView.default.extend({
     defaultTemplate: _emberHtmlbarsTemplatesTopLevelView.default,
@@ -38450,7 +38450,7 @@ enifed('ember-template-compiler/system/compile_options', ['exports', 'ember-meta
     options.buildMeta = function buildMeta(program) {
       return {
         fragmentReason: fragmentReason(program),
-        revision: 'Ember@2.2.0',
+        revision: 'Ember@2.2.1',
         loc: program.loc,
         moduleName: options.moduleName
       };
@@ -43780,7 +43780,7 @@ enifed('ember-views/views/collection_view', ['exports', 'ember-metal/core', 'emb
 enifed('ember-views/views/container_view', ['exports', 'ember-metal/core', 'ember-metal/debug', 'ember-runtime/mixins/mutable_array', 'ember-runtime/system/native_array', 'ember-views/views/view', 'ember-metal/property_get', 'ember-metal/property_set', 'ember-metal/mixin', 'ember-metal/events', 'ember-htmlbars/templates/container-view'], function (exports, _emberMetalCore, _emberMetalDebug, _emberRuntimeMixinsMutable_array, _emberRuntimeSystemNative_array, _emberViewsViewsView, _emberMetalProperty_get, _emberMetalProperty_set, _emberMetalMixin, _emberMetalEvents, _emberHtmlbarsTemplatesContainerView) {
   'use strict';
 
-  _emberHtmlbarsTemplatesContainerView.default.meta.revision = 'Ember@2.2.0';
+  _emberHtmlbarsTemplatesContainerView.default.meta.revision = 'Ember@2.2.1';
 
   /**
   @module ember
@@ -49119,8 +49119,8 @@ enifed('morph-range', ['exports', 'morph-range/utils'], function (exports, _morp
           return this.setNode(content);
         }
         /* Handlebars.SafeString */
-        if (typeof content.string === 'string') {
-          return this.setHTML(content.string);
+        if (typeof content.toHTML === 'function') {
+          return this.setHTML(content.toHTML());
         }
         if (this.parseTextAsHTML) {
           return this.setHTML(content.toString());
