67de8c0c2b200c8a07604d393a507ee32108285d
Tomster <tomster@emberjs.com>
2016-02-21 14:31:18 +0000

    Ember Bower Auto build for https://github.com/emberjs/ember.js/commits/17a94bebbd7eb2ed42841a96bfef9fcdba090ff9.

--- ember-testing.js
+++ ember-testing.js
@@ -6,7 +6,7 @@
  *            Portions Copyright 2008-2011 Apple Inc. All rights reserved.
  * @license   Licensed under MIT license
  *            See https://raw.github.com/emberjs/ember.js/master/LICENSE
- * @version   2.4.0-beta.2+f61014d1
+ * @version   2.4.0-beta.3
  */
 
 var enifed, requireModule, require, requirejs, Ember;
--- ember.prod.js
+++ ember.prod.js
@@ -6,7 +6,7 @@
  *            Portions Copyright 2008-2011 Apple Inc. All rights reserved.
  * @license   Licensed under MIT license
  *            See https://raw.github.com/emberjs/ember.js/master/LICENSE
- * @version   2.4.0-beta.2+f61014d1
+ * @version   2.4.0-beta.3
  */
 
 var enifed, requireModule, require, requirejs, Ember;
@@ -9881,7 +9881,7 @@ enifed('ember-htmlbars/keywords/outlet', ['exports', 'ember-metal/debug', 'ember
 
   'use strict';
 
-  _emberHtmlbarsTemplatesTopLevelView.default.meta.revision = 'Ember@2.4.0-beta.2+f61014d1';
+  _emberHtmlbarsTemplatesTopLevelView.default.meta.revision = 'Ember@2.4.0-beta.3';
 
   /**
     The `{{outlet}}` helper lets you specify where a child routes will render in
@@ -15458,7 +15458,7 @@ enifed('ember-metal/core', ['exports', 'require'], function (exports, _require) 
   
     @class Ember
     @static
-    @version 2.4.0-beta.2+f61014d1
+    @version 2.4.0-beta.3
     @public
   */
 
@@ -15500,11 +15500,11 @@ enifed('ember-metal/core', ['exports', 'require'], function (exports, _require) 
   
     @property VERSION
     @type String
-    @default '2.4.0-beta.2+f61014d1'
+    @default '2.4.0-beta.3'
     @static
     @public
   */
-  Ember.VERSION = '2.4.0-beta.2+f61014d1';
+  Ember.VERSION = '2.4.0-beta.3';
 
   /**
     The hash of environment variables used to control various configuration
@@ -29388,7 +29388,7 @@ enifed('ember-routing-views/components/link-to', ['exports', 'ember-metal/logger
 
   'use strict';
 
-  _emberHtmlbarsTemplatesLinkTo.default.meta.revision = 'Ember@2.4.0-beta.2+f61014d1';
+  _emberHtmlbarsTemplatesLinkTo.default.meta.revision = 'Ember@2.4.0-beta.3';
 
   /**
     `Ember.LinkComponent` renders an element whose `click` event triggers a
@@ -29888,7 +29888,7 @@ enifed('ember-routing-views/views/outlet', ['exports', 'ember-views/views/view',
 
   'use strict';
 
-  _emberHtmlbarsTemplatesTopLevelView.default.meta.revision = 'Ember@2.4.0-beta.2+f61014d1';
+  _emberHtmlbarsTemplatesTopLevelView.default.meta.revision = 'Ember@2.4.0-beta.3';
 
   var CoreOutletView = _emberViewsViewsView.default.extend({
     defaultTemplate: _emberHtmlbarsTemplatesTopLevelView.default,
@@ -38814,7 +38814,7 @@ enifed('ember-template-compiler/system/compile_options', ['exports', 'ember-meta
     options.buildMeta = function buildMeta(program) {
       return {
         fragmentReason: fragmentReason(program),
-        revision: 'Ember@2.4.0-beta.2+f61014d1',
+        revision: 'Ember@2.4.0-beta.3',
         loc: program.loc,
         moduleName: options.moduleName
       };
@@ -42880,7 +42880,7 @@ enifed('ember-views/views/collection_view', ['exports', 'ember-metal/core', 'emb
 enifed('ember-views/views/container_view', ['exports', 'ember-metal/core', 'ember-metal/debug', 'ember-runtime/mixins/mutable_array', 'ember-runtime/system/native_array', 'ember-views/views/view', 'ember-metal/property_get', 'ember-metal/property_set', 'ember-metal/mixin', 'ember-metal/events', 'ember-htmlbars/templates/container-view'], function (exports, _emberMetalCore, _emberMetalDebug, _emberRuntimeMixinsMutable_array, _emberRuntimeSystemNative_array, _emberViewsViewsView, _emberMetalProperty_get, _emberMetalProperty_set, _emberMetalMixin, _emberMetalEvents, _emberHtmlbarsTemplatesContainerView) {
   'use strict';
 
-  _emberHtmlbarsTemplatesContainerView.default.meta.revision = 'Ember@2.4.0-beta.2+f61014d1';
+  _emberHtmlbarsTemplatesContainerView.default.meta.revision = 'Ember@2.4.0-beta.3';
 
   /**
   @module ember
