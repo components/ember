48ece333659b8d8dd87b72d03073a057fb367831
Tomster <tomster@emberjs.com>
2016-03-07 21:38:19 +0000

    Ember Bower Auto build for https://github.com/emberjs/ember.js/commits/4d4951359edb72f3949c6493e61574c8d5ee89f6.

--- ember-runtime.js
+++ ember-runtime.js
@@ -6,7 +6,7 @@
  *            Portions Copyright 2008-2011 Apple Inc. All rights reserved.
  * @license   Licensed under MIT license
  *            See https://raw.github.com/emberjs/ember.js/master/LICENSE
- * @version   2.5.0-beta.1+873a45e1
+ * @version   2.5.0-beta.2
  */
 
 var enifed, requireModule, require, requirejs, Ember;
@@ -4826,7 +4826,7 @@ enifed('ember-metal/core', ['exports', 'require'], function (exports, _require) 
   
     @class Ember
     @static
-    @version 2.5.0-beta.1+873a45e1
+    @version 2.5.0-beta.2
     @public
   */
 
@@ -4868,11 +4868,11 @@ enifed('ember-metal/core', ['exports', 'require'], function (exports, _require) 
   
     @property VERSION
     @type String
-    @default '2.5.0-beta.1+873a45e1'
+    @default '2.5.0-beta.2'
     @static
     @public
   */
-  Ember.VERSION = '2.5.0-beta.1+873a45e1';
+  Ember.VERSION = '2.5.0-beta.2';
 
   /**
     The hash of environment variables used to control various configuration
--- ember-template-compiler.js
+++ ember-template-compiler.js
@@ -6,7 +6,7 @@
  *            Portions Copyright 2008-2011 Apple Inc. All rights reserved.
  * @license   Licensed under MIT license
  *            See https://raw.github.com/emberjs/ember.js/master/LICENSE
- * @version   2.5.0-beta.1+873a45e1
+ * @version   2.5.0-beta.2
  */
 
 var enifed, requireModule, require, requirejs, Ember;
@@ -4052,7 +4052,7 @@ enifed('ember-metal/core', ['exports', 'require'], function (exports, _require) 
   
     @class Ember
     @static
-    @version 2.5.0-beta.1+873a45e1
+    @version 2.5.0-beta.2
     @public
   */
 
@@ -4094,11 +4094,11 @@ enifed('ember-metal/core', ['exports', 'require'], function (exports, _require) 
   
     @property VERSION
     @type String
-    @default '2.5.0-beta.1+873a45e1'
+    @default '2.5.0-beta.2'
     @static
     @public
   */
-  Ember.VERSION = '2.5.0-beta.1+873a45e1';
+  Ember.VERSION = '2.5.0-beta.2';
 
   /**
     The hash of environment variables used to control various configuration
@@ -12710,7 +12710,7 @@ enifed('ember-template-compiler/system/compile_options', ['exports', 'ember-meta
     options.buildMeta = function buildMeta(program) {
       return {
         fragmentReason: fragmentReason(program),
-        revision: 'Ember@2.5.0-beta.1+873a45e1',
+        revision: 'Ember@2.5.0-beta.2',
         loc: program.loc,
         moduleName: options.moduleName
       };
