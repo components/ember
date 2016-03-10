eab1f74cd3e5078b59c9578bde301c98d9885a38
Tomster <tomster@emberjs.com>
2016-03-10 10:47:01 +0000

    Ember Bower Auto build for https://github.com/emberjs/ember.js/commits/4b7a57325fe7dc1e9b7454fe8b1a376295cbad42.

--- ember-runtime.js
+++ ember-runtime.js
@@ -6,7 +6,7 @@
  *            Portions Copyright 2008-2011 Apple Inc. All rights reserved.
  * @license   Licensed under MIT license
  *            See https://raw.github.com/emberjs/ember.js/master/LICENSE
- * @version   2.6.0-canary+551f4626
+ * @version   2.6.0-canary+4b7a5732
  */
 
 var enifed, requireModule, require, requirejs, Ember;
@@ -4826,7 +4826,7 @@ enifed('ember-metal/core', ['exports', 'require'], function (exports, _require) 
   
     @class Ember
     @static
-    @version 2.6.0-canary+551f4626
+    @version 2.6.0-canary+4b7a5732
     @public
   */
 
@@ -4868,11 +4868,11 @@ enifed('ember-metal/core', ['exports', 'require'], function (exports, _require) 
   
     @property VERSION
     @type String
-    @default '2.6.0-canary+551f4626'
+    @default '2.6.0-canary+4b7a5732'
     @static
     @public
   */
-  Ember.VERSION = '2.6.0-canary+551f4626';
+  Ember.VERSION = '2.6.0-canary+4b7a5732';
 
   /**
     The hash of environment variables used to control various configuration
--- ember-tests.prod.js
+++ ember-tests.prod.js
@@ -6,7 +6,7 @@
  *            Portions Copyright 2008-2011 Apple Inc. All rights reserved.
  * @license   Licensed under MIT license
  *            See https://raw.github.com/emberjs/ember.js/master/LICENSE
- * @version   2.6.0-canary+551f4626
+ * @version   2.6.0-canary+4b7a5732
  */
 
 var enifed, requireModule, require, requirejs, Ember;
@@ -34394,7 +34394,7 @@ enifed('ember-htmlbars/tests/helpers/view_test', ['exports', 'ember-metal/core',
     try {
       _emberRuntimeTestsUtils.runAppend(view);
     } catch (error) {
-      equal(error.message, 'Unsupported Content: Cannot bind to function');
+      ok(error.message.indexOf('Unsupported Content: Cannot bind to function') > -1);
     }
   });
 });
@@ -67025,7 +67025,7 @@ enifed('ember-template-compiler/tests/system/compile_test', ['exports', 'ember-t
 
     var actual = _emberTemplateCompilerSystemCompile.default(templateString);
 
-    equal(actual.meta.revision, 'Ember@2.6.0-canary+551f4626', 'revision is included in generated template');
+    equal(actual.meta.revision, 'Ember@2.6.0-canary+4b7a5732', 'revision is included in generated template');
   });
 
   QUnit.test('the template revision is different than the HTMLBars default revision', function () {
