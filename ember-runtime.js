a959cff843748b23e994bcf01ee2636932d87563
Tomster <tomster@emberjs.com>
2016-03-17 19:13:36 +0000

    Ember Bower Auto build for https://github.com/emberjs/ember.js/commits/f786c23dde5afabbc027b31ce569761809e0cfe4.

--- ember-runtime.js
+++ ember-runtime.js
@@ -6,7 +6,7 @@
  *            Portions Copyright 2008-2011 Apple Inc. All rights reserved.
  * @license   Licensed under MIT license
  *            See https://raw.github.com/emberjs/ember.js/master/LICENSE
- * @version   2.6.0-canary+3a99a6dc
+ * @version   2.6.0-canary+f786c23d
  */
 
 var enifed, requireModule, require, requirejs, Ember;
@@ -4826,7 +4826,7 @@ enifed('ember-metal/core', ['exports', 'require'], function (exports, _require) 
   
     @class Ember
     @static
-    @version 2.6.0-canary+3a99a6dc
+    @version 2.6.0-canary+f786c23d
     @public
   */
 
@@ -4868,11 +4868,11 @@ enifed('ember-metal/core', ['exports', 'require'], function (exports, _require) 
   
     @property VERSION
     @type String
-    @default '2.6.0-canary+3a99a6dc'
+    @default '2.6.0-canary+f786c23d'
     @static
     @public
   */
-  Ember.VERSION = '2.6.0-canary+3a99a6dc';
+  Ember.VERSION = '2.6.0-canary+f786c23d';
 
   /**
     The hash of environment variables used to control various configuration
--- ember-template-compiler.js
+++ ember-template-compiler.js
@@ -6,7 +6,7 @@
  *            Portions Copyright 2008-2011 Apple Inc. All rights reserved.
  * @license   Licensed under MIT license
  *            See https://raw.github.com/emberjs/ember.js/master/LICENSE
- * @version   2.6.0-canary+3a99a6dc
+ * @version   2.6.0-canary+f786c23d
  */
 
 var enifed, requireModule, require, requirejs, Ember;
@@ -4060,7 +4060,7 @@ enifed('ember-metal/core', ['exports', 'require'], function (exports, _require) 
   
     @class Ember
     @static
-    @version 2.6.0-canary+3a99a6dc
+    @version 2.6.0-canary+f786c23d
     @public
   */
 
@@ -4102,11 +4102,11 @@ enifed('ember-metal/core', ['exports', 'require'], function (exports, _require) 
   
     @property VERSION
     @type String
-    @default '2.6.0-canary+3a99a6dc'
+    @default '2.6.0-canary+f786c23d'
     @static
     @public
   */
-  Ember.VERSION = '2.6.0-canary+3a99a6dc';
+  Ember.VERSION = '2.6.0-canary+f786c23d';
 
   /**
     The hash of environment variables used to control various configuration
@@ -12711,7 +12711,7 @@ enifed('ember-template-compiler/system/compile_options', ['exports', 'ember-meta
     options.buildMeta = function buildMeta(program) {
       return {
         fragmentReason: fragmentReason(program),
-        revision: 'Ember@2.6.0-canary+3a99a6dc',
+        revision: 'Ember@2.6.0-canary+f786c23d',
         loc: program.loc,
         moduleName: options.moduleName
       };
