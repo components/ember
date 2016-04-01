007da8b4666537b3645a45ad3d056dd8f7b9484c
Tomster <tomster@emberjs.com>
2016-04-01 01:07:49 +0000

    Ember Bower Auto build for https://github.com/emberjs/ember.js/commits/57cf48cfce2acd895572f6f4c0b9478490259cdc.

--- ember-runtime.js
+++ ember-runtime.js
@@ -6,7 +6,7 @@
  *            Portions Copyright 2008-2011 Apple Inc. All rights reserved.
  * @license   Licensed under MIT license
  *            See https://raw.github.com/emberjs/ember.js/master/LICENSE
- * @version   2.6.0-canary+3d4fddec
+ * @version   2.6.0-canary+57cf48cf
  */
 
 var enifed, requireModule, require, requirejs, Ember;
@@ -4832,7 +4832,7 @@ enifed('ember-metal/core', ['exports', 'require'], function (exports, _require) 
   
     @class Ember
     @static
-    @version 2.6.0-canary+3d4fddec
+    @version 2.6.0-canary+57cf48cf
     @public
   */
 
@@ -4874,11 +4874,11 @@ enifed('ember-metal/core', ['exports', 'require'], function (exports, _require) 
   
     @property VERSION
     @type String
-    @default '2.6.0-canary+3d4fddec'
+    @default '2.6.0-canary+57cf48cf'
     @static
     @public
   */
-  Ember.VERSION = '2.6.0-canary+3d4fddec';
+  Ember.VERSION = '2.6.0-canary+57cf48cf';
 
   /**
     The hash of environment variables used to control various configuration
