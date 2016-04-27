9a1dae5508885b3f767a1247810387ff4ab4a4e6
Tomster <tomster@emberjs.com>
2016-04-27 18:53:17 +0000

    Ember Bower Auto build for https://github.com/emberjs/ember.js/commits/2779fd6fce56b3e51ff40e16225376a7f5f78327.

--- ember-runtime.js
+++ ember-runtime.js
@@ -6,7 +6,7 @@
  *            Portions Copyright 2008-2011 Apple Inc. All rights reserved.
  * @license   Licensed under MIT license
  *            See https://raw.github.com/emberjs/ember.js/master/LICENSE
- * @version   2.7.0-canary+5332777f
+ * @version   2.7.0-canary+2779fd6f
  */
 
 var enifed, requireModule, require, Ember;
@@ -4243,7 +4243,7 @@ enifed('ember-metal/core', ['exports', 'require', 'ember-environment'], function
   
     @class Ember
     @static
-    @version 2.7.0-canary+5332777f
+    @version 2.7.0-canary+2779fd6f
     @public
   */
   var Ember = typeof _emberEnvironment.context.imports.Ember === 'object' && _emberEnvironment.context.imports.Ember || {};
@@ -4270,11 +4270,11 @@ enifed('ember-metal/core', ['exports', 'require', 'ember-environment'], function
   
     @property VERSION
     @type String
-    @default '2.7.0-canary+5332777f'
+    @default '2.7.0-canary+2779fd6f'
     @static
     @public
   */
-  Ember.VERSION = '2.7.0-canary+5332777f';
+  Ember.VERSION = '2.7.0-canary+2779fd6f';
 
   // ..........................................................
   // BOOTSTRAP
--- ember.prod.js
+++ ember.prod.js
@@ -6,7 +6,7 @@
  *            Portions Copyright 2008-2011 Apple Inc. All rights reserved.
  * @license   Licensed under MIT license
  *            See https://raw.github.com/emberjs/ember.js/master/LICENSE
- * @version   2.7.0-canary+5332777f
+ * @version   2.7.0-canary+2779fd6f
  */
 
 var enifed, requireModule, require, Ember;
@@ -12499,7 +12499,7 @@ enifed('ember-htmlbars/keywords/outlet', ['exports', 'ember-metal/debug', 'ember
   'use strict';
 
   if (!_emberMetalFeatures.default('ember-glimmer')) {
-    _emberHtmlbarsTemplatesTopLevelView.default.meta.revision = 'Ember@2.7.0-canary+5332777f';
+    _emberHtmlbarsTemplatesTopLevelView.default.meta.revision = 'Ember@2.7.0-canary+2779fd6f';
   }
 
   /**
@@ -16639,7 +16639,7 @@ enifed('ember-metal/core', ['exports', 'require', 'ember-environment'], function
   
     @class Ember
     @static
-    @version 2.7.0-canary+5332777f
+    @version 2.7.0-canary+2779fd6f
     @public
   */
   var Ember = typeof _emberEnvironment.context.imports.Ember === 'object' && _emberEnvironment.context.imports.Ember || {};
@@ -16666,11 +16666,11 @@ enifed('ember-metal/core', ['exports', 'require', 'ember-environment'], function
   
     @property VERSION
     @type String
-    @default '2.7.0-canary+5332777f'
+    @default '2.7.0-canary+2779fd6f'
     @static
     @public
   */
-  Ember.VERSION = '2.7.0-canary+5332777f';
+  Ember.VERSION = '2.7.0-canary+2779fd6f';
 
   // ..........................................................
   // BOOTSTRAP
@@ -40957,7 +40957,7 @@ enifed('ember-template-compiler/system/compile_options', ['exports', 'ember-meta
     options.buildMeta = function buildMeta(program) {
       return {
         fragmentReason: fragmentReason(program),
-        revision: 'Ember@2.7.0-canary+5332777f',
+        revision: 'Ember@2.7.0-canary+2779fd6f',
         loc: program.loc,
         moduleName: options.moduleName
       };
@@ -49031,7 +49031,7 @@ enifed("glimmer/index", ["exports"], function (exports) {
  * @copyright Copyright 2011-2015 Tilde Inc. and contributors
  * @license   Licensed under MIT license
  *            See https://raw.githubusercontent.com/tildeio/glimmer/master/LICENSE
- * @version   2.7.0-canary+5332777f
+ * @version   2.7.0-canary+2779fd6f
  */
 //# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbImdsaW1tZXIvaW5kZXgudHMiXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6IiIsImZpbGUiOiJpbmRleC5qcyIsInNvdXJjZXNDb250ZW50IjpbXX0=
 enifed('glimmer-reference/index', ['exports', 'glimmer-reference/lib/reference', 'glimmer-reference/lib/const', 'glimmer-reference/lib/validators', 'glimmer-reference/lib/utils', 'glimmer-reference/lib/iterable'], function (exports, _glimmerReferenceLibReference, _glimmerReferenceLibConst, _glimmerReferenceLibValidators, _glimmerReferenceLibUtils, _glimmerReferenceLibIterable) {
