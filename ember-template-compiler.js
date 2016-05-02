85adf49026d9789df22b1b7429426c138b97bd09
Tomster <tomster@emberjs.com>
2016-05-02 18:52:40 +0000

    Ember Bower Auto build for https://github.com/emberjs/ember.js/commits/103e4b7000fc4067c47ef8442835ce5d3b24c4fd.

--- ember-template-compiler.js
+++ ember-template-compiler.js
@@ -6,7 +6,7 @@
  *            Portions Copyright 2008-2011 Apple Inc. All rights reserved.
  * @license   Licensed under MIT license
  *            See https://raw.github.com/emberjs/ember.js/master/LICENSE
- * @version   2.7.0-canary+0e0909f7
+ * @version   2.7.0-canary+103e4b70
  */
 
 var enifed, requireModule, require, Ember;
@@ -3614,7 +3614,7 @@ enifed('ember-metal/core', ['exports', 'require', 'ember-environment'], function
   
     @class Ember
     @static
-    @version 2.7.0-canary+0e0909f7
+    @version 2.7.0-canary+103e4b70
     @public
   */
   var Ember = typeof _emberEnvironment.context.imports.Ember === 'object' && _emberEnvironment.context.imports.Ember || {};
@@ -3641,11 +3641,11 @@ enifed('ember-metal/core', ['exports', 'require', 'ember-environment'], function
   
     @property VERSION
     @type String
-    @default '2.7.0-canary+0e0909f7'
+    @default '2.7.0-canary+103e4b70'
     @static
     @public
   */
-  Ember.VERSION = '2.7.0-canary+0e0909f7';
+  Ember.VERSION = '2.7.0-canary+103e4b70';
 
   // ..........................................................
   // BOOTSTRAP
@@ -12244,7 +12244,7 @@ enifed('ember-template-compiler/system/compile_options', ['exports', 'ember-meta
     options.buildMeta = function buildMeta(program) {
       return {
         fragmentReason: fragmentReason(program),
-        revision: 'Ember@2.7.0-canary+0e0909f7',
+        revision: 'Ember@2.7.0-canary+103e4b70',
         loc: program.loc,
         moduleName: options.moduleName
       };
--- ember-tests.prod.js
+++ ember-tests.prod.js
@@ -6,7 +6,7 @@
  *            Portions Copyright 2008-2011 Apple Inc. All rights reserved.
  * @license   Licensed under MIT license
  *            See https://raw.github.com/emberjs/ember.js/master/LICENSE
- * @version   2.7.0-canary+0e0909f7
+ * @version   2.7.0-canary+103e4b70
  */
 
 var enifed, requireModule, require, Ember;
@@ -81316,7 +81316,7 @@ enifed('ember-template-compiler/tests/system/compile_test', ['exports', 'ember-t
 
       var actual = _emberTemplateCompilerSystemCompile.default(templateString);
 
-      equal(actual.meta.revision, 'Ember@2.7.0-canary+0e0909f7', 'revision is included in generated template');
+      equal(actual.meta.revision, 'Ember@2.7.0-canary+103e4b70', 'revision is included in generated template');
     });
 
     QUnit.test('the template revision is different than the HTMLBars default revision', function () {
