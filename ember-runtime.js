3cfd7cce49a7b3ac5ae0997f244bdcfef8018fd0
Tomster <tomster@emberjs.com>
2016-05-03 15:54:45 +0000

    Ember Bower Auto build for https://github.com/emberjs/ember.js/commits/715fb540124a655ef55666ec6244b52fff5369bb.

--- ember-runtime.js
+++ ember-runtime.js
@@ -6,7 +6,7 @@
  *            Portions Copyright 2008-2011 Apple Inc. All rights reserved.
  * @license   Licensed under MIT license
  *            See https://raw.github.com/emberjs/ember.js/master/LICENSE
- * @version   2.7.0-canary+48f7b813
+ * @version   2.7.0-canary+715fb540
  */
 
 var enifed, requireModule, require, Ember;
@@ -4391,7 +4391,7 @@ enifed('ember-metal/core', ['exports', 'require', 'ember-environment'], function
   
     @class Ember
     @static
-    @version 2.7.0-canary+48f7b813
+    @version 2.7.0-canary+715fb540
     @public
   */
   var Ember = typeof _emberEnvironment.context.imports.Ember === 'object' && _emberEnvironment.context.imports.Ember || {};
@@ -4418,11 +4418,11 @@ enifed('ember-metal/core', ['exports', 'require', 'ember-environment'], function
   
     @property VERSION
     @type String
-    @default '2.7.0-canary+48f7b813'
+    @default '2.7.0-canary+715fb540'
     @static
     @public
   */
-  Ember.VERSION = '2.7.0-canary+48f7b813';
+  Ember.VERSION = '2.7.0-canary+715fb540';
 
   // ..........................................................
   // BOOTSTRAP
--- ember-testing.js
+++ ember-testing.js
@@ -6,7 +6,7 @@
  *            Portions Copyright 2008-2011 Apple Inc. All rights reserved.
  * @license   Licensed under MIT license
  *            See https://raw.github.com/emberjs/ember.js/master/LICENSE
- * @version   2.7.0-canary+48f7b813
+ * @version   2.7.0-canary+715fb540
  */
 
 var enifed, requireModule, require, Ember;
