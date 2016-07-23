a25b13e29d124a9197ec62caf34d2a56abba5510
Tomster <tomster@emberjs.com>
2016-07-23 12:10:20 +0000

    Ember Bower Auto build for https://github.com/emberjs/ember.js/commits/75447393b3b45a9a62f0f207636faa8db1123ab1.

--- ember-template-compiler.js
+++ ember-template-compiler.js
@@ -6,7 +6,7 @@
  *            Portions Copyright 2008-2011 Apple Inc. All rights reserved.
  * @license   Licensed under MIT license
  *            See https://raw.github.com/emberjs/ember.js/master/LICENSE
- * @version   2.7.0-canary+818b52cd
+ * @version   2.7.0-canary+75447393
  */
 
 var enifed, requireModule, require, Ember;
@@ -12048,7 +12048,7 @@ enifed("ember/features", ["exports"], function (exports) {
 enifed("ember/version", ["exports"], function (exports) {
   "use strict";
 
-  exports.default = "2.7.0-canary+818b52cd";
+  exports.default = "2.7.0-canary+75447393";
 });
 enifed("htmlbars-compiler", ["exports", "htmlbars-compiler/compiler"], function (exports, _htmlbarsCompilerCompiler) {
   "use strict";
