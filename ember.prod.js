c8ce51be171e2e8e8b10c16c2e01b2958595e426
Tomster <tomster@emberjs.com>
2016-07-27 14:49:22 +0000

    Ember Bower Auto build for https://github.com/emberjs/ember.js/commit/b1ccb543117017ad2b1f16e053cc310d62b4ee6b.

--- ember.prod.js
+++ ember.prod.js
@@ -10146,7 +10146,7 @@ enifed('ember-glimmer/renderer', ['exports', 'ember-glimmer/utils/references', '
 
   var runInTransaction = undefined;
 
-  if (false || true) {
+  if (false || false) {
     runInTransaction = _emberMetalTransaction.default;
   } else {
     runInTransaction = function (callback) {
@@ -12043,7 +12043,7 @@ enifed('ember-glimmer/utils/references', ['exports', 'ember-metal/property_get',
 
   var TwoWayFlushDetectionTag = undefined;
 
-  if (false || true) {
+  if (false || false) {
     TwoWayFlushDetectionTag = (function () {
       function _class(tag, key, ref) {
         _classCallCheck(this, _class);
@@ -12097,7 +12097,7 @@ enifed('ember-glimmer/utils/references', ['exports', 'ember-metal/property_get',
       this._parentObjectTag = parentObjectTag;
       this._propertyKey = propertyKey;
 
-      if (false || true) {
+      if (false || false) {
         var tag = _glimmerReference.combine([parentReferenceTag, parentObjectTag]);
         this.tag = new TwoWayFlushDetectionTag(tag, propertyKey, this);
       } else {
@@ -12124,7 +12124,7 @@ enifed('ember-glimmer/utils/references', ['exports', 'ember-metal/property_get',
           _emberMetalWatch_key.watchKey(parentValue, _propertyKey, meta);
         }
 
-        if (false || true) {
+        if (false || false) {
           this.tag.didCompute(parentValue);
         }
 
@@ -25911,7 +25911,7 @@ enifed('ember-metal/meta', ['exports', 'ember-metal/features', 'ember-metal/meta
     tag: ownCustomObject
   };
 
-  if (false || true) {
+  if (false || false) {
     members.lastRendered = ownMap;
     members.lastRenderedFrom = ownMap; // FIXME: not used in production, remove me from prod builds
   }
@@ -25944,7 +25944,7 @@ enifed('ember-metal/meta', ['exports', 'ember-metal/features', 'ember-metal/meta
     // inherited, and we can optimize it much better than JS runtimes.
     this.parent = parentMeta;
 
-    if (false || true) {
+    if (false || false) {
       this._lastRendered = undefined;
       this._lastRenderedFrom = undefined; // FIXME: not used in production, remove me from prod builds
     }
@@ -27951,7 +27951,7 @@ enifed('ember-metal/property_events', ['exports', 'ember-metal/utils', 'ember-me
 
     _emberMetalTags.markObjectAsDirty(m);
 
-    if (false || true) {
+    if (false || false) {
       _emberMetalTransaction.assertNotRendered(obj, keyName, m);
     }
   }
@@ -29261,14 +29261,14 @@ enifed('ember-metal/transaction', ['exports', 'ember-metal/meta', 'ember-metal/d
       didRender = undefined,
       assertNotRendered = undefined;
 
-  if (false || true) {}
+  if (false || false) {}
 
   var raise = _emberMetalDebug.assert;
-  if (true) {
+  if (false) {
     raise = function (message, test) {};
   }
 
-  if (false || true) {
+  if (false || false) {
     (function () {
       var counter = 0;
       var inTransaction = false;
