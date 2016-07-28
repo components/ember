40b095d623201373310999d98846c4b14b4ecdfc
Tomster <tomster@emberjs.com>
2016-07-28 15:37:05 +0000

    Ember Bower Auto build for https://github.com/emberjs/ember.js/commit/0628013c10d07060d18aa18b7bc31ce01a400ab0.

--- ember-tests.prod.js
+++ ember-tests.prod.js
@@ -12438,7 +12438,7 @@ enifed('ember-glimmer/tests/integration/components/curly-components-test', ['exp
     _class.prototype['@glimmer component should rerender when a property is changed during children\'s rendering'] = function glimmerComponentShouldRerenderWhenAPropertyIsChangedDuringChildrenSRendering(assert) {
       var _this58 = this;
 
-      if (false) {
+      if (true) {
         (function () {
           expectDeprecation(/modified value twice in a single render/);
           var outer = undefined,
@@ -12517,7 +12517,7 @@ enifed('ember-glimmer/tests/integration/components/curly-components-test', ['exp
     _class.prototype['@glimmer asserts when a property is changed during children\'s rendering'] = function glimmerAssertsWhenAPropertyIsChangedDuringChildrenSRendering(assert) {
       var _this59 = this;
 
-      if (!false) {
+      if (!true) {
         (function () {
           var outer = undefined,
               middle = undefined;
@@ -12580,7 +12580,7 @@ enifed('ember-glimmer/tests/integration/components/curly-components-test', ['exp
     _class.prototype['@glimmer component should rerender when a shared dependency is changed during children\'s rendering'] = function glimmerComponentShouldRerenderWhenASharedDependencyIsChangedDuringChildrenSRendering(assert) {
       var _this60 = this;
 
-      if (false) {
+      if (true) {
         (function () {
           expectDeprecation(/modified wrapper.content twice in a single render/);
           var outer = undefined,
@@ -12720,7 +12720,7 @@ enifed('ember-glimmer/tests/integration/components/curly-components-test', ['exp
     _class.prototype['@glimmer asserts when a shared dependency is changed during children\'s rendering'] = function glimmerAssertsWhenASharedDependencyIsChangedDuringChildrenSRendering(assert) {
       var _this62 = this;
 
-      if (!false) {
+      if (!true) {
         var outer = undefined,
             middle = undefined;
 
@@ -35631,7 +35631,7 @@ enifed('ember-htmlbars/tests/integration/components/curly-components-test', ['ex
     _class.prototype['@glimmer component should rerender when a property is changed during children\'s rendering'] = function glimmerComponentShouldRerenderWhenAPropertyIsChangedDuringChildrenSRendering(assert) {
       var _this58 = this;
 
-      if (false) {
+      if (true) {
         (function () {
           expectDeprecation(/modified value twice in a single render/);
           var outer = undefined,
@@ -35710,7 +35710,7 @@ enifed('ember-htmlbars/tests/integration/components/curly-components-test', ['ex
     _class.prototype['@glimmer asserts when a property is changed during children\'s rendering'] = function glimmerAssertsWhenAPropertyIsChangedDuringChildrenSRendering(assert) {
       var _this59 = this;
 
-      if (!false) {
+      if (!true) {
         (function () {
           var outer = undefined,
               middle = undefined;
@@ -35773,7 +35773,7 @@ enifed('ember-htmlbars/tests/integration/components/curly-components-test', ['ex
     _class.prototype['@glimmer component should rerender when a shared dependency is changed during children\'s rendering'] = function glimmerComponentShouldRerenderWhenASharedDependencyIsChangedDuringChildrenSRendering(assert) {
       var _this60 = this;
 
-      if (false) {
+      if (true) {
         (function () {
           expectDeprecation(/modified wrapper.content twice in a single render/);
           var outer = undefined,
@@ -35913,7 +35913,7 @@ enifed('ember-htmlbars/tests/integration/components/curly-components-test', ['ex
     _class.prototype['@glimmer asserts when a shared dependency is changed during children\'s rendering'] = function glimmerAssertsWhenASharedDependencyIsChangedDuringChildrenSRendering(assert) {
       var _this62 = this;
 
-      if (!false) {
+      if (!true) {
         var outer = undefined,
             middle = undefined;
 
