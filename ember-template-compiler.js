46af5862e9fdcf9ff22516ecae1e0dc504e75769
Tomster <tomster@emberjs.com>
2016-07-29 23:34:25 +0000

    Ember Bower Auto build for https://github.com/emberjs/ember.js/commit/b236ff1ea5004f249ae3fd57c2053d3b40a4c964.

--- ember-template-compiler.js
+++ ember-template-compiler.js
@@ -6,7 +6,7 @@
  *            Portions Copyright 2008-2011 Apple Inc. All rights reserved.
  * @license   Licensed under MIT license
  *            See https://raw.github.com/emberjs/ember.js/master/LICENSE
- * @version   2.9.0-null+f247628d
+ * @version   2.9.0-null+b236ff1e
  */
 
 var enifed, requireModule, require, Ember;
@@ -10058,6 +10058,13 @@ enifed('ember-metal/transaction', ['exports', 'ember-metal/meta', 'ember-metal/d
     };
   }
 
+  var implication = undefined;
+  if (false) {
+    implication = 'will be removed in Ember 3.0.';
+  } else if (true) {
+    implication = 'is no longer supported. See https://github.com/emberjs/ember.js/issues/13948 for more details.';
+  }
+
   if (true || false) {
     (function () {
       var counter = 0;
@@ -10097,12 +10104,20 @@ enifed('ember-metal/transaction', ['exports', 'ember-metal/meta', 'ember-metal/d
             var parts = [];
             var lastRef = ref[key];
 
-            while (lastRef && lastRef._propertyKey && lastRef._parentReference) {
-              parts.unshift(lastRef._propertyKey);
-              lastRef = lastRef._parentReference;
+            var label = undefined;
+
+            if (lastRef) {
+              while (lastRef && lastRef._propertyKey && lastRef._parentReference) {
+                parts.unshift(lastRef._propertyKey);
+                lastRef = lastRef._parentReference;
+              }
+
+              label = parts.join();
+            } else {
+              label = 'the same value';
             }
 
-            return 'You modified ' + parts.join('.') + ' twice in a single render. This was unreliable and slow in Ember 1.x and will be removed in Ember 3.0.';
+            return 'You modified ' + parts.join('.') + ' twice in a single render. This was unreliable and slow in Ember 1.x and ' + implication;
           })(), false);
 
           shouldReflush = true;
@@ -12195,7 +12210,7 @@ enifed("ember/features", ["exports"], function (exports) {
 enifed("ember/version", ["exports"], function (exports) {
   "use strict";
 
-  exports.default = "2.9.0-null+f247628d";
+  exports.default = "2.9.0-null+b236ff1e";
 });
 enifed("htmlbars-compiler", ["exports", "htmlbars-compiler/compiler"], function (exports, _htmlbarsCompilerCompiler) {
   "use strict";
