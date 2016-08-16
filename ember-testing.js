f9b7d1f455aa7bb3e800e02c6d45bb13b34ec222
Robert Jackson <rwjblue@twitch.tv>
2016-08-16 08:41:22 -0400

    Release v2.9.0-alpha.4.

--- ember-testing.js
+++ ember-testing.js
@@ -6,7 +6,7 @@
  *            Portions Copyright 2008-2011 Apple Inc. All rights reserved.
  * @license   Licensed under MIT license
  *            See https://raw.github.com/emberjs/ember.js/master/LICENSE
- * @version   2.9.0-alpha.3
+ * @version   2.9.0-alpha.4
  */
 
 var enifed, requireModule, require, Ember;
