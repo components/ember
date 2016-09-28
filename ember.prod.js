af134fdb505134b2d8c0d1fb8f954ca918e4604d
Tomster <tomster@emberjs.com>
2016-09-28 14:57:28 +0000

    Ember Bower Auto build for https://github.com/emberjs/ember.js/commit/778ee33b9d7287d7925387abb5eedcb42acb1758.

--- ember.prod.js
+++ ember.prod.js
@@ -6,7 +6,7 @@
  *            Portions Copyright 2008-2011 Apple Inc. All rights reserved.
  * @license   Licensed under MIT license
  *            See https://raw.github.com/emberjs/ember.js/master/LICENSE
- * @version   2.10.0-canary+c5086616
+ * @version   2.10.0-canary+778ee33b
  */
 
 var enifed, requireModule, require, Ember;
@@ -39348,7 +39348,7 @@ enifed('ember/index', ['exports', 'require', 'ember-environment', 'ember-utils',
 enifed("ember/version", ["exports"], function (exports) {
   "use strict";
 
-  exports.default = "2.10.0-canary+c5086616";
+  exports.default = "2.10.0-canary+778ee33b";
 });
 enifed('internal-test-helpers/factory', ['exports'], function (exports) {
   'use strict';
