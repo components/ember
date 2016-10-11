a831c2303746912dfea9bc6383c15de29360a737
Tomster <tomster@emberjs.com>
2016-10-11 19:56:17 +0000

    Ember Bower Auto build for https://github.com/emberjs/ember.js/commit/7c5205c34bd990aa4db8590c73e67ee9a11c2adb.

--- ember-runtime.js
+++ ember-runtime.js
@@ -6,7 +6,7 @@
  *            Portions Copyright 2008-2011 Apple Inc. All rights reserved.
  * @license   Licensed under MIT license
  *            See https://raw.github.com/emberjs/ember.js/master/LICENSE
- * @version   2.9.0-beta.5-beta+78885cdb
+ * @version   2.9.0-beta.5-beta+7c5205c3
  */
 
 var enifed, requireModule, require, Ember;
@@ -19100,7 +19100,7 @@ enifed("ember/features", ["exports"], function (exports) {
 enifed("ember/version", ["exports"], function (exports) {
   "use strict";
 
-  exports.default = "2.9.0-beta.5-beta+78885cdb";
+  exports.default = "2.9.0-beta.5-beta+7c5205c3";
 });
 /*!
  * @overview RSVP - a tiny implementation of Promises/A+.
--- ember.prod.js
+++ ember.prod.js
@@ -6,7 +6,7 @@
  *            Portions Copyright 2008-2011 Apple Inc. All rights reserved.
  * @license   Licensed under MIT license
  *            See https://raw.github.com/emberjs/ember.js/master/LICENSE
- * @version   2.9.0-beta.5-beta+78885cdb
+ * @version   2.9.0-beta.5-beta+7c5205c3
  */
 
 var enifed, requireModule, require, Ember;
@@ -37965,7 +37965,7 @@ enifed('ember/index', ['exports', 'require', 'ember-environment', 'ember-utils',
 enifed("ember/version", ["exports"], function (exports) {
   "use strict";
 
-  exports.default = "2.9.0-beta.5-beta+78885cdb";
+  exports.default = "2.9.0-beta.5-beta+7c5205c3";
 });
 enifed('internal-test-helpers/factory', ['exports'], function (exports) {
   'use strict';
