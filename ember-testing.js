5bbc3d47f312dd9939c4ec2da2328b85f5bfbc32
Tomster <tomster@emberjs.com>
2016-10-15 00:46:20 +0000

    Ember Bower Auto build for https://github.com/emberjs/ember.js/commit/45f1416d8a4fdf66102c0fb959087d3c733d0f21.

--- ember-testing.js
+++ ember-testing.js
@@ -6,7 +6,7 @@
  *            Portions Copyright 2008-2011 Apple Inc. All rights reserved.
  * @license   Licensed under MIT license
  *            See https://raw.github.com/emberjs/ember.js/master/LICENSE
- * @version   2.10.0-alpha.1-canary+900c6fbe
+ * @version   2.10.0-alpha.1-canary+45f1416d
  */
 
 var enifed, requireModule, require, Ember;
