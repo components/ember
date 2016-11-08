7213bc02aaad7ea6ef9374ee3dc7e04fe7b29b0e
Tomster <tomster@emberjs.com>
2016-11-08 13:58:01 +0000

    Ember Bower Auto build for https://github.com/emberjs/ember.js/commit/edd39023dd4a3edc6a07724b8fedc33974c4201c.

--- ember-runtime.js
+++ ember-runtime.js
@@ -6,7 +6,7 @@
  *            Portions Copyright 2008-2011 Apple Inc. All rights reserved.
  * @license   Licensed under MIT license
  *            See https://raw.github.com/emberjs/ember.js/master/LICENSE
- * @version   2.11.0-alpha.1-canary+5cd78794
+ * @version   2.11.0-alpha.1-canary+edd39023
  */
 
 var enifed, requireModule, Ember;
@@ -19403,7 +19403,7 @@ enifed("ember/features", ["exports"], function (exports) {
 enifed("ember/version", ["exports"], function (exports) {
   "use strict";
 
-  exports.default = "2.11.0-alpha.1-canary+5cd78794";
+  exports.default = "2.11.0-alpha.1-canary+edd39023";
 });
 enifed('rsvp', ['exports'], function (exports) {
   'use strict';
--- ember.prod.js
+++ ember.prod.js
@@ -6,7 +6,7 @@
  *            Portions Copyright 2008-2011 Apple Inc. All rights reserved.
  * @license   Licensed under MIT license
  *            See https://raw.github.com/emberjs/ember.js/master/LICENSE
- * @version   2.11.0-alpha.1-canary+5cd78794
+ * @version   2.11.0-alpha.1-canary+edd39023
  */
 
 var enifed, requireModule, Ember;
@@ -9291,8 +9291,8 @@ enifed('ember-glimmer/helpers/get', ['exports', 'ember-metal', 'ember-glimmer/ut
   
     ```handlebars
     {{input value=(mut (get person factName)) type="text"}}
-    <button {{action (mut factName) "height"}}>Show height</button>
-    <button {{action (mut factName) "weight"}}>Show weight</button>
+    <button {{action (action (mut factName)) "height"}}>Show height</button>
+    <button {{action (action (mut factName)) "weight"}}>Show weight</button>
     ```
   
     Would allow the user to swap what fact is being displayed, and also edit
@@ -39678,7 +39678,7 @@ enifed('ember/index', ['exports', 'require', 'ember-environment', 'ember-utils',
 enifed("ember/version", ["exports"], function (exports) {
   "use strict";
 
-  exports.default = "2.11.0-alpha.1-canary+5cd78794";
+  exports.default = "2.11.0-alpha.1-canary+edd39023";
 });
 enifed('internal-test-helpers/apply-mixins', ['exports', 'ember-utils'], function (exports, _emberUtils) {
   'use strict';
