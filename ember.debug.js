45ab65fa70d48a4e3c9e7b0c35eae506f2975d20
Tomster <tomster@emberjs.com>
2016-11-11 20:27:34 +0000

    Ember Bower Auto build for https://github.com/emberjs/ember.js/commit/f62e055f2f8a89b3073a258ef07129741f887b89.

--- ember.debug.js
+++ ember.debug.js
@@ -6,7 +6,7 @@
  *            Portions Copyright 2008-2011 Apple Inc. All rights reserved.
  * @license   Licensed under MIT license
  *            See https://raw.github.com/emberjs/ember.js/master/LICENSE
- * @version   2.11.0-alpha.1-canary+d28d4d29
+ * @version   2.11.0-alpha.1-canary+f62e055f
  */
 
 var enifed, requireModule, Ember;
@@ -22897,7 +22897,7 @@ enifed('ember-routing/ext/controller', ['exports', 'ember-metal', 'ember-runtime
       while transitioning to the route.
       @for Ember.ControllerMixin
       @method replaceRoute
-      @private
+      @public
     */
     replaceRoute: function () {
       // target may be either another controller or a router
@@ -42675,7 +42675,7 @@ enifed('ember/index', ['exports', 'require', 'ember-environment', 'ember-utils',
 enifed("ember/version", ["exports"], function (exports) {
   "use strict";
 
-  exports.default = "2.11.0-alpha.1-canary+d28d4d29";
+  exports.default = "2.11.0-alpha.1-canary+f62e055f";
 });
 enifed('internal-test-helpers/apply-mixins', ['exports', 'ember-utils'], function (exports, _emberUtils) {
   'use strict';
