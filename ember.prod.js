71b688a05d35b8efab7ab9ee130d5d6851cd2349
Tomster <tomster@emberjs.com>
2016-11-27 10:23:06 +0000

    Ember Bower Auto build for https://github.com/emberjs/ember.js/commit/fbaca746d83026e6ed4f765549bfb681dfcded53.

--- ember.prod.js
+++ ember.prod.js
@@ -6,7 +6,7 @@
  *            Portions Copyright 2008-2011 Apple Inc. All rights reserved.
  * @license   Licensed under MIT license
  *            See https://raw.github.com/emberjs/ember.js/master/LICENSE
- * @version   2.10.0-beta.3-beta+7847c187
+ * @version   2.10.0-beta.3-beta+fbaca746
  */
 
 var enifed, requireModule, require, Ember;
@@ -9740,6 +9740,101 @@ enifed('ember-glimmer/helpers/readonly', ['exports', 'ember-glimmer/utils/refere
   @submodule ember-glimmer
   */
   'use strict';
+
+  /**
+    The `readonly` helper let's you specify that a binding is one-way only,
+    instead of two-way.
+    When you pass a `readonly` binding from an outer context (e.g. parent component),
+    to to an inner context (e.g. child component), you are saying that changing that
+    property in the inner context does not change the value in the outer context.
+  
+    To specify that a binding is read-only, when invoking the child `Component`:
+  
+    ```app/components/my-parent.js
+    export default Component.extend({
+      totalClicks: 3
+    });
+    ```
+  
+    ```app/templates/components/my-parent.hbs
+    {{log totalClicks}} // -> 3
+    {{my-child childClickCount=(readonly totalClicks)}}
+    ```
+  
+    Now, when you update `childClickCount`:
+  
+    ```app/components/my-child.js
+    export default Component.extend({
+      click() {
+        this.incrementProperty('childClickCount');
+      }
+    });
+    ```
+  
+    The value updates in the child component, but not the parent component:
+  
+    ```app/templates/components/my-child.hbs
+    {{log childClickCount}} //-> 4
+    ```
+  
+    ```app/templates/components/my-parent.hbs
+    {{log totalClicks}} //-> 3
+    {{my-child childClickCount=(readonly totalClicks)}}
+    ```
+  
+    ### Objects and Arrays
+  
+    When passing a property that is a complex object (e.g. object, array) instead of a primitive object (e.g. number, string),
+    only the reference to the object is protected using the readonly helper.
+    This means that you can change properties of the object both on the parent component, as well as the child component.
+    The `readonly` binding behaves similar to the `const` keyword in JavaScript.
+  
+    Let's look at an example:
+  
+    First let's set up the parent component:
+  
+    ```app/components/my-parent.js
+    export default Ember.Component.extend({
+      clicks: null,
+  
+      init() {
+        this._super(...arguments);
+        this.set('clicks', { total: 3 });
+      }
+    });
+    ```
+  
+    ```app/templates/components/my-parent.hbs
+    {{log clicks.total}} //-> 3
+    {{my-child childClicks=(readonly clicks)}}
+    ```
+  
+    Now, if you update the `total` property of `childClicks`:
+  
+    ```app/components/my-child.js
+    export default Ember.Component.extend({
+      click() {
+        this.get('clicks').incrementProperty('total');
+      }
+    });
+    ```
+  
+    You will see the following happen:
+  
+    ```app/templates/components/my-parent.hbs
+    {{log clicks.total}} //-> 4
+    {{my-child childClicks=(readonly clicks)}}
+    ```
+  
+    ```app/templates/components/my-child.hbs
+    {{log childClicks.total}} //-> 4
+    ```
+  
+    @method readonly
+    @param {Object} [attr] the read-only attribute.
+    @for Ember.Templates.helpers
+    @private
+  */
 
   exports.default = function (vm, args) {
     var ref = _emberGlimmerHelpersMut.unMut(args.positional.at(0));
@@ -21525,13 +21620,31 @@ enifed('ember-routing/ext/controller', ['exports', 'ember-metal', 'ember-runtime
     queryParams: null,
 
     /**
-      @property _qpDelegate
+     This property is updated to various different callback functions depending on
+     the current "state" of the backing route. It is used by
+     `Ember.Controller.prototype._qpChanged`.
+      The methods backing each state can be found in the `Ember.Route.prototype._qp` computed
+     property return value (the `.states` property). The current values are listed here for
+     the sanity of future travelers:
+      * `inactive` - This state is used when this controller instance is not part of the active
+       route heirarchy. Set in `Ember.Route.prototype._reset` (a `router.js` microlib hook) and
+       `Ember.Route.prototype.actions.finalizeQueryParamChange`.
+     * `active` - This state is used when this controller instance is part of the active
+       route heirarchy. Set in `Ember.Route.prototype.actions.finalizeQueryParamChange`.
+     * `allowOverrides` - This state is used in `Ember.Route.prototype.setup` (`route.js` microlib hook).
+       @method _qpDelegate
       @private
     */
     _qpDelegate: null, // set by route
 
     /**
-      @method _qpChanged
+     During `Ember.Route#setup` observers are created to invoke this method
+     when any of the query params declared in `Ember.Controller#queryParams` property
+     are changed.
+       When invoked this method uses the currently active query param update delegate
+     (see `Ember.Controller.prototype._qpDelegate` for details) and invokes it with
+     the QP key/value being changed.
+       @method _qpChanged
       @private
     */
     _qpChanged: function (controller, _prop) {
@@ -21654,7 +21767,7 @@ enifed('ember-routing/ext/controller', ['exports', 'ember-metal', 'ember-runtime
       while transitioning to the route.
       @for Ember.ControllerMixin
       @method replaceRoute
-      @private
+      @public
     */
     replaceRoute: function () {
       // target may be either another controller or a router
@@ -39544,7 +39657,7 @@ enifed('ember/index', ['exports', 'require', 'ember-environment', 'ember-utils',
 enifed("ember/version", ["exports"], function (exports) {
   "use strict";
 
-  exports.default = "2.10.0-beta.3-beta+7847c187";
+  exports.default = "2.10.0-beta.3-beta+fbaca746";
 });
 enifed('internal-test-helpers/apply-mixins', ['exports', 'ember-utils'], function (exports, _emberUtils) {
   'use strict';
