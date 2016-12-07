63c6d43c6c78c3a92e218b00862468590da29af0
Tomster <tomster@emberjs.com>
2016-12-07 21:50:06 +0000

    Ember Bower Auto build for https://github.com/emberjs/ember.js/commit/aa18288f9aca194b6a89d7075f1d37eb34aa4c0e.

--- ember-runtime.js
+++ ember-runtime.js
@@ -6,7 +6,7 @@
  *            Portions Copyright 2008-2011 Apple Inc. All rights reserved.
  * @license   Licensed under MIT license
  *            See https://raw.github.com/emberjs/ember.js/master/LICENSE
- * @version   2.10.0-release+76680c61
+ * @version   2.10.0-release+aa18288f
  */
 
 var enifed, requireModule, Ember;
@@ -19413,7 +19413,7 @@ enifed("ember/features", ["exports"], function (exports) {
 enifed("ember/version", ["exports"], function (exports) {
   "use strict";
 
-  exports.default = "2.10.0-release+76680c61";
+  exports.default = "2.10.0-release+aa18288f";
 });
 /*!
  * @overview RSVP - a tiny implementation of Promises/A+.
