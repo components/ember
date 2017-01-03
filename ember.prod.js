1d284cf855e922c7e81479e442c63705a8b79c47
Tomster <tomster@emberjs.com>
2017-01-03 17:14:33 +0000

    Ember Bower Auto build for https://github.com/emberjs/ember.js/commit/7ee964337cae4d8e0c72fc1d5ef35b5893ca9ada.

--- ember.prod.js
+++ ember.prod.js
@@ -6,7 +6,7 @@
  *            Portions Copyright 2008-2011 Apple Inc. All rights reserved.
  * @license   Licensed under MIT license
  *            See https://raw.github.com/emberjs/ember.js/master/LICENSE
- * @version   2.12.0-alpha.1-canary+ea632bf5
+ * @version   2.12.0-alpha.1-canary+7ee96433
  */
 
 var enifed, requireModule, Ember;
@@ -7142,7 +7142,7 @@ enifed('ember-glimmer/components/link-to', ['exports', 'ember-console', 'ember-m
     @namespace Ember
     @extends Ember.Component
     @see {Ember.Templates.helpers.link-to}
-    @private
+    @public
   **/
   var LinkComponent = _emberGlimmerComponent.default.extend({
     layout: _emberGlimmerTemplatesLinkTo.default,
@@ -40035,7 +40035,7 @@ enifed('ember/index', ['exports', 'require', 'ember-environment', 'ember-utils',
 enifed("ember/version", ["exports"], function (exports) {
   "use strict";
 
-  exports.default = "2.12.0-alpha.1-canary+ea632bf5";
+  exports.default = "2.12.0-alpha.1-canary+7ee96433";
 });
 enifed('internal-test-helpers/apply-mixins', ['exports', 'ember-utils'], function (exports, _emberUtils) {
   'use strict';
