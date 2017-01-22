4a787207b8119ea27677d6246fe416b555704b42
Tomster <tomster@emberjs.com>
2017-01-22 18:46:16 +0000

    Ember Bower Auto build for https://github.com/emberjs/ember.js/commit/85900d9a46800e425df19b99b4c97284adf7049a.

--- ember-template-compiler.js
+++ ember-template-compiler.js
@@ -6,7 +6,7 @@
  *            Portions Copyright 2008-2011 Apple Inc. All rights reserved.
  * @license   Licensed under MIT license
  *            See https://raw.github.com/emberjs/ember.js/master/LICENSE
- * @version   2.12.0-alpha.1-canary+70a94bb2
+ * @version   2.12.0-alpha.1-canary+85900d9a
  */
 
 var enifed, requireModule, Ember;
@@ -15989,7 +15989,7 @@ enifed("ember/features", ["exports"], function (exports) {
 enifed("ember/version", ["exports"], function (exports) {
   "use strict";
 
-  exports.default = "2.12.0-alpha.1-canary+70a94bb2";
+  exports.default = "2.12.0-alpha.1-canary+85900d9a";
 });
 enifed("handlebars", ["exports"], function (exports) {
   /* istanbul ignore next */
--- ember.js
+++ ember.js
@@ -6,7 +6,7 @@
  *            Portions Copyright 2008-2011 Apple Inc. All rights reserved.
  * @license   Licensed under MIT license
  *            See https://raw.github.com/emberjs/ember.js/master/LICENSE
- * @version   2.12.0-alpha.1-canary+70a94bb2
+ * @version   2.12.0-alpha.1-canary+85900d9a
  */
 
 var enifed, requireModule, Ember;
@@ -45182,7 +45182,7 @@ enifed('ember/index', ['exports', 'require', 'ember-environment', 'ember-utils',
 enifed("ember/version", ["exports"], function (exports) {
   "use strict";
 
-  exports.default = "2.12.0-alpha.1-canary+70a94bb2";
+  exports.default = "2.12.0-alpha.1-canary+85900d9a";
 });
 enifed('internal-test-helpers/apply-mixins', ['exports', 'ember-utils'], function (exports, _emberUtils) {
   'use strict';
