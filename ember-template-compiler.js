cb7c738d467b5009b2ce3cf1c65a81a183b2c086
Tomster <tomster@emberjs.com>
2017-02-01 03:00:18 +0000

    Ember Bower Auto build for https://github.com/emberjs/ember.js/commit/4bf1a29fa82fb1da94cee11448a336eb41dd1914.

--- ember-template-compiler.js
+++ ember-template-compiler.js
@@ -6,7 +6,7 @@
  *            Portions Copyright 2008-2011 Apple Inc. All rights reserved.
  * @license   Licensed under MIT license
  *            See https://raw.github.com/emberjs/ember.js/master/LICENSE
- * @version   2.13.0-alpha.1-canary+d5364f96
+ * @version   2.13.0-alpha.1-canary+4bf1a29f
  */
 
 var enifed, requireModule, Ember;
@@ -15977,7 +15977,7 @@ enifed("ember/features", ["exports"], function (exports) {
 enifed("ember/version", ["exports"], function (exports) {
   "use strict";
 
-  exports.default = "2.13.0-alpha.1-canary+d5364f96";
+  exports.default = "2.13.0-alpha.1-canary+4bf1a29f";
 });
 enifed("handlebars", ["exports"], function (exports) {
   /* istanbul ignore next */
--- ember.prod.js
+++ ember.prod.js
@@ -6,7 +6,7 @@
  *            Portions Copyright 2008-2011 Apple Inc. All rights reserved.
  * @license   Licensed under MIT license
  *            See https://raw.github.com/emberjs/ember.js/master/LICENSE
- * @version   2.13.0-alpha.1-canary+d5364f96
+ * @version   2.13.0-alpha.1-canary+4bf1a29f
  */
 
 var enifed, requireModule, Ember;
@@ -11059,8 +11059,10 @@ enifed('ember-glimmer/helpers/component', ['exports', 'ember-utils', 'ember-glim
 
     function ClosureComponentReference(args, symbolTable, env) {
       _CachedReference.call(this);
-      this.defRef = args.positional.at(0);
-      this.tag = this.defRef.tag;
+
+      var firstArg = args.positional.at(0);
+      this.defRef = firstArg;
+      this.tag = firstArg.tag;
       this.env = env;
       this.symbolTable = symbolTable;
       this.args = args;
@@ -41592,7 +41594,7 @@ enifed('ember/index', ['exports', 'require', 'ember-environment', 'ember-utils',
 enifed("ember/version", ["exports"], function (exports) {
   "use strict";
 
-  exports.default = "2.13.0-alpha.1-canary+d5364f96";
+  exports.default = "2.13.0-alpha.1-canary+4bf1a29f";
 });
 enifed('internal-test-helpers/apply-mixins', ['exports', 'ember-utils'], function (exports, _emberUtils) {
   'use strict';
