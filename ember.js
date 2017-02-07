67b8a234a03598a4c11fe0b20021c76cbff7a8e4
Tomster <tomster@emberjs.com>
2017-02-07 14:57:06 +0000

    Ember Bower Auto build for https://github.com/emberjs/ember.js/commit/76698a45efe30a9391cd4759222d6c72b3054c5b.

--- ember.js
+++ ember.js
@@ -6,7 +6,7 @@
  *            Portions Copyright 2008-2011 Apple Inc. All rights reserved.
  * @license   Licensed under MIT license
  *            See https://raw.github.com/emberjs/ember.js/master/LICENSE
- * @version   2.13.0-alpha.1-canary+3bd80eef
+ * @version   2.13.0-alpha.1-canary+76698a45
  */
 
 var enifed, requireModule, Ember;
@@ -45262,7 +45262,7 @@ enifed('ember/index', ['exports', 'require', 'ember-environment', 'ember-utils',
 enifed("ember/version", ["exports"], function (exports) {
   "use strict";
 
-  exports.default = "2.13.0-alpha.1-canary+3bd80eef";
+  exports.default = "2.13.0-alpha.1-canary+76698a45";
 });
 enifed('internal-test-helpers/apply-mixins', ['exports', 'ember-utils'], function (exports, _emberUtils) {
   'use strict';
