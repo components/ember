3f9112a198e035d155f20906dd02ffab14ae9acd
Tomster <tomster@emberjs.com>
2017-03-07 19:40:26 +0000

    Ember Bower Auto build for https://github.com/emberjs/ember.js/commit/2bcb190f4fceeede2be19f6b13d064406ca04489.

--- ember-tests.prod.js
+++ ember-tests.prod.js
@@ -6,7 +6,7 @@
  *            Portions Copyright 2008-2011 Apple Inc. All rights reserved.
  * @license   Licensed under MIT license
  *            See https://raw.github.com/emberjs/ember.js/master/LICENSE
- * @version   2.12.0-beta.2-beta+e50ab58c
+ * @version   2.12.0-beta.2-beta+2bcb190f
  */
 
 var enifed, requireModule, Ember;
@@ -164,7 +164,7 @@ function defaults(obj, defaults) {
   return obj;
 }
 
-babelHelpers = {
+var babelHelpers = {
   inherits: inherits,
   taggedTemplateLiteralLoose: taggedTemplateLiteralLoose,
   slice: Array.prototype.slice,
--- ember.js
+++ ember.js
@@ -6,7 +6,7 @@
  *            Portions Copyright 2008-2011 Apple Inc. All rights reserved.
  * @license   Licensed under MIT license
  *            See https://raw.github.com/emberjs/ember.js/master/LICENSE
- * @version   2.12.0-beta.2-beta+e50ab58c
+ * @version   2.12.0-beta.2-beta+2bcb190f
  */
 
 var enifed, requireModule, Ember;
@@ -174,7 +174,7 @@ function defaults(obj, defaults) {
   return obj;
 }
 
-babelHelpers = {
+var babelHelpers = {
   classCallCheck: classCallCheck,
   inherits: inherits,
   taggedTemplateLiteralLoose: taggedTemplateLiteralLoose,
@@ -43525,7 +43525,7 @@ enifed('ember/index', ['exports', 'require', 'ember-environment', 'ember-utils',
 enifed("ember/version", ["exports"], function (exports) {
   "use strict";
 
-  exports.default = "2.12.0-beta.2-beta+e50ab58c";
+  exports.default = "2.12.0-beta.2-beta+2bcb190f";
 });
 enifed('internal-test-helpers/apply-mixins', ['exports', 'ember-utils'], function (exports, _emberUtils) {
   'use strict';
