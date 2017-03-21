028afd7c6623d6050485b4720dcb7423709fd406
Tomster <tomster@emberjs.com>
2017-03-21 17:52:12 +0000

    Ember Bower Auto build for https://github.com/emberjs/ember.js/commit/5401a89e2a651c8cfda9a05ce0b4c20e28069944.

--- ember-runtime.js
+++ ember-runtime.js
@@ -6,7 +6,7 @@
  *            Portions Copyright 2008-2011 Apple Inc. All rights reserved.
  * @license   Licensed under MIT license
  *            See https://raw.github.com/emberjs/ember.js/master/LICENSE
- * @version   2.14.0-alpha.1-null+09474073
+ * @version   2.14.0-alpha.1-null+5401a89e
  */
 
 var enifed, requireModule, Ember;
--- ember-template-compiler.js
+++ ember-template-compiler.js
@@ -6,7 +6,7 @@
  *            Portions Copyright 2008-2011 Apple Inc. All rights reserved.
  * @license   Licensed under MIT license
  *            See https://raw.github.com/emberjs/ember.js/master/LICENSE
- * @version   2.14.0-alpha.1-null+09474073
+ * @version   2.14.0-alpha.1-null+5401a89e
  */
 
 var enifed, requireModule, Ember;
@@ -24677,7 +24677,7 @@ enifed("ember/features", ["exports"], function (exports) {
 enifed("ember/version", ["exports"], function (exports) {
   "use strict";
 
-  exports.default = "2.14.0-alpha.1-null+09474073";
+  exports.default = "2.14.0-alpha.1-null+5401a89e";
 });
 
 enifed("handlebars", ["exports"], function (exports) {
