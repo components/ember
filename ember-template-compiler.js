9158dd6515bcf7dfc1a82ab9b54cf27d0ebeb6e7
Tomster <tomster@emberjs.com>
2017-05-11 21:36:22 +0000

    Ember Bower Auto build for https://github.com/emberjs/ember.js/commit/6e8e474c72f6dee5ca6a27f6e7c7f98e5f579587.

--- ember-template-compiler.js
+++ ember-template-compiler.js
@@ -6,7 +6,7 @@
  *            Portions Copyright 2008-2011 Apple Inc. All rights reserved.
  * @license   Licensed under MIT license
  *            See https://raw.github.com/emberjs/ember.js/master/LICENSE
- * @version   2.15.0-alpha.1-null+00e51255
+ * @version   2.15.0-alpha.1-null+6e8e474c
  */
 
 var enifed, requireModule, Ember;
@@ -16458,7 +16458,7 @@ enifed('ember/features', ['exports', 'ember-environment', 'ember-utils'], functi
 enifed("ember/version", ["exports"], function (exports) {
   "use strict";
 
-  exports.default = "2.15.0-alpha.1-null+00e51255";
+  exports.default = "2.15.0-alpha.1-null+6e8e474c";
 });
 enifed("handlebars", ["exports"], function (exports) {
   "use strict";
--- ember.debug.js
+++ ember.debug.js
@@ -6,7 +6,7 @@
  *            Portions Copyright 2008-2011 Apple Inc. All rights reserved.
  * @license   Licensed under MIT license
  *            See https://raw.github.com/emberjs/ember.js/master/LICENSE
- * @version   2.15.0-alpha.1-null+00e51255
+ * @version   2.15.0-alpha.1-null+6e8e474c
  */
 
 var enifed, requireModule, Ember;
@@ -47002,7 +47002,7 @@ enifed('ember-views/views/core_view', ['exports', 'ember-runtime', 'ember-views/
       }
     },
     has: function (name) {
-      return (0, _emberRuntime.typeOf)(this[name]) === 'function' || this._super(name);
+      return typeof this[name] === 'function' || this._super(name);
     }
   });
 
@@ -47743,7 +47743,7 @@ enifed('ember/index', ['exports', 'require', 'ember-environment', 'node-module',
 enifed("ember/version", ["exports"], function (exports) {
   "use strict";
 
-  exports.default = "2.15.0-alpha.1-null+00e51255";
+  exports.default = "2.15.0-alpha.1-null+6e8e474c";
 });
 enifed("handlebars", ["exports"], function (exports) {
   "use strict";
