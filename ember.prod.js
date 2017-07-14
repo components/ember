7e7bff14205680680741ffb833341a7feee3803f
Tomster <tomster@emberjs.com>
2017-07-14 09:15:26 +0000

    Ember Bower Auto build for https://github.com/emberjs/ember.js/commit/cb97ca5f780aa9013e78ba772ea3f53f293b40e0.

--- ember.prod.js
+++ ember.prod.js
@@ -6,7 +6,7 @@
  *            Portions Copyright 2008-2011 Apple Inc. All rights reserved.
  * @license   Licensed under MIT license
  *            See https://raw.github.com/emberjs/ember.js/master/LICENSE
- * @version   2.16.0-alpha.1-null+cc044a37
+ * @version   2.16.0-alpha.1-null+cb97ca5f
  */
 
 var enifed, requireModule, Ember;
@@ -18344,11 +18344,11 @@ enifed('ember-glimmer/helpers/get', ['exports', 'ember-babel', 'ember-metal', 'e
   }(_references.CachedReference);
 });
 enifed("ember-glimmer/helpers/hash", ["exports"], function (exports) {
-   "use strict";
+  "use strict";
 
-   exports.default = function (vm, args) {
-      return args.named.capture();
-   };
+  exports.default = function (vm, args) {
+    return args.named.capture();
+  };
 });
 enifed('ember-glimmer/helpers/if-unless', ['exports', 'ember-babel', 'ember-debug', 'ember-glimmer/utils/references', '@glimmer/reference'], function (exports, _emberBabel, _emberDebug, _references, _reference) {
   'use strict';
@@ -44168,7 +44168,7 @@ enifed('ember/index', ['exports', 'require', 'ember-environment', 'node-module',
 enifed("ember/version", ["exports"], function (exports) {
   "use strict";
 
-  exports.default = "2.16.0-alpha.1-null+cc044a37";
+  exports.default = "2.16.0-alpha.1-null+cb97ca5f";
 });
 enifed('node-module', ['exports'], function(_exports) {
   var IS_NODE = typeof module === 'object' && typeof module.require === 'function';
