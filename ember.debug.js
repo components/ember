bb3f45de1a9b120bf8609c1207c64059b0b4f821
Tomster <tomster@emberjs.com>
2017-07-21 08:42:27 +0000

    Ember Bower Auto build for https://github.com/emberjs/ember.js/commit/e3c7509ea76b10592927f60e9cf6ed37ae6a93ad.

--- ember.debug.js
+++ ember.debug.js
@@ -6,7 +6,7 @@
  *            Portions Copyright 2008-2011 Apple Inc. All rights reserved.
  * @license   Licensed under MIT license
  *            See https://raw.github.com/emberjs/ember.js/master/LICENSE
- * @version   2.15.0-beta.1-null+9568aa2e
+ * @version   2.15.0-beta.2
  */
 
 var enifed, requireModule, Ember;
@@ -48183,7 +48183,7 @@ enifed('ember/index', ['exports', 'require', 'ember-environment', 'node-module',
 enifed("ember/version", ["exports"], function (exports) {
   "use strict";
 
-  exports.default = "2.15.0-beta.1-null+9568aa2e";
+  exports.default = "2.15.0-beta.2";
 });
 enifed("handlebars", ["exports"], function (exports) {
   "use strict";
