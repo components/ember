d2fae53cf48e72a6474d3cb17697d360ab8e4a62
Tomster <tomster@emberjs.com>
2017-10-10 13:19:18 +0000

    Ember Bower Auto build for https://github.com/emberjs/ember.js/commit/f7fa758b275bf69e30761651562e3acf268a7b1f.

--- ember.prod.js
+++ ember.prod.js
@@ -6,7 +6,7 @@
  *            Portions Copyright 2008-2011 Apple Inc. All rights reserved.
  * @license   Licensed under MIT license
  *            See https://raw.github.com/emberjs/ember.js/master/LICENSE
- * @version   2.17.0-alpha.1-null+8d5ee89c
+ * @version   2.17.0-alpha.1-null+f7fa758b
  */
 
 var enifed, requireModule, Ember;
@@ -11436,7 +11436,6 @@ enifed("dag-map", ["exports"], function (exports) {
         };
         return IntStack;
     }();
-    //# sourceMappingURL=dag-map.js.map
 });
 enifed('ember-application/index', ['exports', 'ember-application/system/application', 'ember-application/system/application-instance', 'ember-application/system/resolver', 'ember-application/system/engine', 'ember-application/system/engine-instance', 'ember-application/system/engine-parent', 'ember-application/initializers/dom-templates'], function (exports, _application, _applicationInstance, _resolver, _engine, _engineInstance, _engineParent) {
   'use strict';
@@ -18357,7 +18356,7 @@ enifed('ember-glimmer/helpers/get', ['exports', 'ember-babel', 'ember-metal', 'e
       var path = this.lastPath = this.pathReference.value();
 
       if (path !== lastPath) {
-        if (path !== undefined && path !== null) {
+        if (path !== undefined && path !== null && path !== '') {
           pathType = typeof path;
 
 
@@ -44383,7 +44382,7 @@ enifed('ember/index', ['exports', 'require', 'ember-environment', 'node-module',
 enifed("ember/version", ["exports"], function (exports) {
   "use strict";
 
-  exports.default = "2.17.0-alpha.1-null+8d5ee89c";
+  exports.default = "2.17.0-alpha.1-null+f7fa758b";
 });
 enifed('node-module', ['exports'], function(_exports) {
   var IS_NODE = typeof module === 'object' && typeof module.require === 'function';
