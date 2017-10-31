bf54c5d088cf94df76c5a6418a975fd920bbf0dc
Tomster <tomster@emberjs.com>
2017-10-31 21:31:55 +0000

    Ember Bower Auto build for https://github.com/emberjs/ember.js/commit/535631a3edcbdfa5c0702d67e62898434b1be7fe.

--- ember-template-compiler.js
+++ ember-template-compiler.js
@@ -6,7 +6,7 @@
  *            Portions Copyright 2008-2011 Apple Inc. All rights reserved.
  * @license   Licensed under MIT license
  *            See https://raw.github.com/emberjs/ember.js/master/LICENSE
- * @version   2.16.1-null+ef999898
+ * @version   2.16.1-null+535631a3
  */
 
 var enifed, requireModule, Ember;
@@ -17293,7 +17293,7 @@ enifed('ember/features', ['exports', 'ember-environment', 'ember-utils'], functi
 enifed("ember/version", ["exports"], function (exports) {
   "use strict";
 
-  exports.default = "2.16.1-null+ef999898";
+  exports.default = "2.16.1-null+535631a3";
 });
 enifed("handlebars", ["exports"], function (exports) {
   "use strict";
--- ember.prod.js
+++ ember.prod.js
@@ -6,7 +6,7 @@
  *            Portions Copyright 2008-2011 Apple Inc. All rights reserved.
  * @license   Licensed under MIT license
  *            See https://raw.github.com/emberjs/ember.js/master/LICENSE
- * @version   2.16.1-null+ef999898
+ * @version   2.16.1-null+535631a3
  */
 
 var enifed, requireModule, Ember;
@@ -44546,7 +44546,7 @@ enifed('ember/index', ['exports', 'require', 'ember-environment', 'node-module',
 enifed("ember/version", ["exports"], function (exports) {
   "use strict";
 
-  exports.default = "2.16.1-null+ef999898";
+  exports.default = "2.16.1-null+535631a3";
 });
 enifed('node-module', ['exports'], function(_exports) {
   var IS_NODE = typeof module === 'object' && typeof module.require === 'function';
