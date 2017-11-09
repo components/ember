bc94eb88ef496059f507a20d20adc7f985c6f7df
Tomster <tomster@emberjs.com>
2017-11-09 02:50:23 +0000

    Ember Bower Auto build for https://github.com/emberjs/ember.js/commit/c1a892dd08099ff757d7f95e96f74c2775a467ad.

--- ember-runtime.js
+++ ember-runtime.js
@@ -6,7 +6,7 @@
  *            Portions Copyright 2008-2011 Apple Inc. All rights reserved.
  * @license   Licensed under MIT license
  *            See https://raw.github.com/emberjs/ember.js/master/LICENSE
- * @version   2.17.0-alpha.1-null+112922b5
+ * @version   2.17.0-alpha.1-null+c1a892dd
  */
 
 /*global process */
--- ember.prod.js
+++ ember.prod.js
@@ -6,7 +6,7 @@
  *            Portions Copyright 2008-2011 Apple Inc. All rights reserved.
  * @license   Licensed under MIT license
  *            See https://raw.github.com/emberjs/ember.js/master/LICENSE
- * @version   2.17.0-alpha.1-null+112922b5
+ * @version   2.17.0-alpha.1-null+c1a892dd
  */
 
 /*global process */
@@ -43951,7 +43951,7 @@ enifed('ember/index', ['exports', 'require', 'ember-environment', 'node-module',
 enifed("ember/version", ["exports"], function (exports) {
   "use strict";
 
-  exports.default = "2.17.0-alpha.1-null+112922b5";
+  exports.default = "2.17.0-alpha.1-null+c1a892dd";
 });
 /*global enifed */
 enifed('node-module', ['exports'], function(_exports) {
