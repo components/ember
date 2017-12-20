c0d9e61db62ff86b80100d8e5cfabeb86f819411
Tomster <tomster@emberjs.com>
2017-12-20 01:40:32 +0000

    Ember Bower Auto build for https://github.com/emberjs/ember.js/commit/e9f4c50efa447e214973f69dd0a22cf0934518d6.

--- ember-template-compiler.js
+++ ember-template-compiler.js
@@ -6,7 +6,7 @@
  *            Portions Copyright 2008-2011 Apple Inc. All rights reserved.
  * @license   Licensed under MIT license
  *            See https://raw.github.com/emberjs/ember.js/master/LICENSE
- * @version   2.18.0-beta.3-null+64e0968d
+ * @version   2.18.0-beta.4
  */
 
 /*global process */
@@ -17346,7 +17346,7 @@ enifed('ember/features', ['exports', 'ember-environment', 'ember-utils'], functi
 enifed("ember/version", ["exports"], function (exports) {
   "use strict";
 
-  exports.default = "2.18.0-beta.3-null+64e0968d";
+  exports.default = "2.18.0-beta.4";
 });
 enifed("handlebars", ["exports"], function (exports) {
   "use strict";
--- ember.prod.js
+++ ember.prod.js
@@ -6,7 +6,7 @@
  *            Portions Copyright 2008-2011 Apple Inc. All rights reserved.
  * @license   Licensed under MIT license
  *            See https://raw.github.com/emberjs/ember.js/master/LICENSE
- * @version   2.18.0-beta.3-null+64e0968d
+ * @version   2.18.0-beta.4
  */
 
 /*global process */
@@ -43876,7 +43876,7 @@ enifed('ember/index', ['exports', 'require', 'ember-environment', 'node-module',
 enifed("ember/version", ["exports"], function (exports) {
   "use strict";
 
-  exports.default = "2.18.0-beta.3-null+64e0968d";
+  exports.default = "2.18.0-beta.4";
 });
 /*global enifed */
 enifed('node-module', ['exports'], function(_exports) {
