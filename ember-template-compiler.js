fc313af8eb45869bb14391f59e244f6f7325745c
Tomster <tomster@emberjs.com>
2017-12-27 21:49:32 +0000

    Ember Bower Auto build for https://github.com/emberjs/ember.js/commit/ba86a0b655f3d1bbe647e2ad355aeeea09244ab7.

--- ember-template-compiler.js
+++ ember-template-compiler.js
@@ -6,7 +6,7 @@
  *            Portions Copyright 2008-2011 Apple Inc. All rights reserved.
  * @license   Licensed under MIT license
  *            See https://raw.github.com/emberjs/ember.js/master/LICENSE
- * @version   2.18.0-beta.4-null+dc2b9fc8
+ * @version   2.18.0-beta.5
  */
 
 /*global process */
@@ -17346,7 +17346,7 @@ enifed('ember/features', ['exports', 'ember-environment', 'ember-utils'], functi
 enifed("ember/version", ["exports"], function (exports) {
   "use strict";
 
-  exports.default = "2.18.0-beta.4-null+dc2b9fc8";
+  exports.default = "2.18.0-beta.5";
 });
 enifed("handlebars", ["exports"], function (exports) {
   "use strict";
--- ember-tests.prod.js
+++ ember-tests.prod.js
@@ -6,7 +6,7 @@
  *            Portions Copyright 2008-2011 Apple Inc. All rights reserved.
  * @license   Licensed under MIT license
  *            See https://raw.github.com/emberjs/ember.js/master/LICENSE
- * @version   2.18.0-beta.4-null+dc2b9fc8
+ * @version   2.18.0-beta.5
  */
 
 /*global process */
